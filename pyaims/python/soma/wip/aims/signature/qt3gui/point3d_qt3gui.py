7783daade97e0ef9090b7ac0e59450aae1f3c20e
denisri <denis.riviere.gm@gmail.com>
2009-10-07 08:50:24 +0000

    CeCILL-B licence headers
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/aims/aims-free/branches/3.2@35029 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- pyaims/python/soma/wip/aims/signature/qt3gui/point3d_qt3gui.py
+++ pyaims/python/soma/wip/aims/signature/qt3gui/point3d_qt3gui.py
@@ -1,12 +1,17 @@
 # -*- coding: iso-8859-1 -*-
 
-#  This software and supporting documentation were developed by
-#  NeuroSpin and IFR 49
+# Copyright IFR 49 (1995-2009)
 #
-# This software is governed by the CeCILL license version 2 under 
+#  This software and supporting documentation were developed by
+#      Institut Federatif de Recherche 49
+#      CEA/NeuroSpin, Batiment 145,
+#      91191 Gif-sur-Yvette cedex
+#      France
+#
+# This software is governed by the CeCILL-B license under
 # French law and abiding by the rules of distribution of free software.
 # You can  use, modify and/or redistribute the software under the 
-# terms of the CeCILL license version 2 as circulated by CEA, CNRS
+# terms of the CeCILL-B license as circulated by CEA, CNRS
 # and INRIA at the following URL "http://www.cecill.info". 
 # 
 # As a counterpart to the access to the source code and  rights to copy,
@@ -27,7 +32,7 @@
 # same conditions as regards security. 
 # 
 # The fact that you are presently reading this means that you have had
-# knowledge of the CeCILL license version 2 and that you accept its terms.
+# knowledge of the CeCILL-B license and that you accept its terms.
 
 '''
 @author: Nicoles Souedet
