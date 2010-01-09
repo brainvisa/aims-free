f5fac28ace02c7690be51c08f2c40534239acfd7
denisri <denis.riviere.gm@gmail.com>
2010-01-09 23:52:17 +0000

    Fix in FSL normalization matrix conversion: The former hypothesis on FSL matrix source/dest referentials proved to be wrong in some cases... I'm trying a new hyothesis which works with images in several different orientations so it might be OK this time.
    Fixes: #901
    
    
    
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/aims/aims-free/branches/3.2@36205 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- pyaims/python/soma/aims/fslTransformation.py
+++ pyaims/python/soma/aims/fslTransformation.py
@@ -38,9 +38,19 @@
 # which appears to be Y and Z flipped compared to Aims
 
 from soma import aims
-import numpy, types
+from soma.minf.api import readMinf
+import numpy, types, os
 
 def fslMatToTrm( matfile, srcimage, dstimage ):
+  '''
+  As far as I have understood:
+  A FSL transformation goes from the disk referential of the source image
+  to the disk referential of the destination image.
+  BUT:
+  if the qform of an image (disk -> "real world") implies a flip (goes from a
+  direct referential to an indirect one or the contrary), then a flip along X
+  axis is inserted in the matrix, since FSL flirt doesn't allow flipping.
+  '''
   if type( srcimage ) in types.StringTypes:
     f = aims.Finder()
     f.check( srcimage )
@@ -60,25 +70,59 @@ def fslMatToTrm( matfile, srcimage, dstimage ):
     except: # otherwise it is considered to already be a header
       im2 = dstimage
 
-  height1 = im1['voxel_size'][1] * ( im1['volume_dimension'][1] - 1 )
-  height2 = im2['voxel_size'][1] * ( im2['volume_dimension'][1] - 1 )
-  size1 = im1['voxel_size'][2] * ( im1['volume_dimension'][2] - 1 )
-  size2 = im2['voxel_size'][2] * ( im2['volume_dimension'][2] - 1 )
+  vs1 = im1['voxel_size'][:3]
+  vs2 = im2['voxel_size'][:3]
+
+  if os.path.exists( matfile + '.minf' ):
+    matminf = readMinf( matfile + '.minf' )[0]
+  else:
+    matminf = {}
+  s2m1 = matminf.get( 'source_storage_to_memory', None )
+  s2m2 = matminf.get( 'destination_storage_to_memory', None )
+  if not s2m1:
+    try:
+      s2m1 = im1[ 'storage_to_memory' ]
+    except:
+      s2m1 = [ -1.,0,0,im1['volume_dimension'][0] - 1,
+        0,-1.,0,im1['volume_dimension'][1] - 1,
+        0,0,-1.,im1['volume_dimension'][2] - 1,
+        0,0,0,1. ]
+  if not s2m2:
+    try:
+      s2m2 = im2[ 'storage_to_memory' ]
+    except:
+      s2m2 = [ 1.,0,0,0,
+        0,-1.,0,im2['volume_dimension'][1] - 1,
+        0,0,-1.,im2['volume_dimension'][2] - 1,
+        0,0,0,1. ]
+  s2m1 = aims.Motion( s2m1 )
+  s2m2 = aims.Motion( s2m2 )
+  vsm1 = aims.Motion()
+  vsm1.fromMatrix( numpy.diag( vs1 + [0] ) )
+  vsm2 = aims.Motion()
+  vsm2.fromMatrix( numpy.diag( vs2 + [0] ) )
+  m2s1 = s2m1.inverse()
+  m2s2 = s2m2.inverse()
+  x = [ abs(x) for x in m2s1.transform( vs1 ) - m2s1.transform( [ 0,0,0 ]) ]
+  vsd1 = aims.Motion()
+  vsd1.fromMatrix( numpy.diag( x + [0] ) )
+  x = [ abs(x) for x in m2s2.transform( vs2 ) - m2s2.transform( [ 0,0,0 ]) ]
+  vsd2 = aims.Motion()
+  vsd2.fromMatrix( numpy.diag( x + [0] ) )
   flip1 = aims.Motion()
-  flip1.rotation().setValue( -1., 1,1 )
-  flip1.translation()[1] = height1
-  flip1.rotation().setValue( -1., 2,2 )
-  flip1.translation()[2] = size1
   flip2 = aims.Motion()
-  flip2.rotation().setValue( -1., 1,1 )
-  flip2.translation()[1] = height2
-  flip2.rotation().setValue( -1., 2,2 )
-  flip2.translation()[2] = size2
+  if not s2m1.isDirect():
+    flip1.rotation().setValue( -1., 0,0 )
+    flip1.translation()[0] = vs1[0] * ( im1['volume_dimension'][0] - 1 )
+  if not s2m2.isDirect():
+    flip2.rotation().setValue( -1., 0,0 )
+    flip2.translation()[0] = vs2[0] * ( im2['volume_dimension'][0] - 1 )
 
   mat = [ x.split() for x in open( matfile ).readlines() ]
   mot = aims.Motion()
   mot.fromMatrix( numpy.mat( mat ) )
 
-  trm = flip2.inverse() * mot * flip1
+  trm = vsm2 * s2m2 * vsd2.inverse() * flip2.inverse() * mot \
+    * flip1 * vsd1 * m2s1 * vsm1.inverse()
 
   return trm
