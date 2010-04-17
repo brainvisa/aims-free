5c488d8dca0e4670f4b67c2c44207f6fec87e572
denisri <denis.riviere.gm@gmail.com>
2010-04-17 16:00:58 +0000

    plugins loader manipulation example
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/aims/aims-free/trunk@37290 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ pyaims/doc/examples/advanced/pluginstricks.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+'''Maniupulate the plugins loader to avoid loading some of them, or to load additional plugins afterwards
+'''
+from soma import aims
+
+dicomfile = 'toto.dcm'
+
+pl = aims.carto.PluginLoader
+pluginfiles = pl.pluginFiles()
+
+# remove aimsdata plugins
+todel = []
+i = 0
+for f in pluginfiles:
+  if f.filename.endswith( 'aimsdata.plugins' ):
+    todel.append( i )
+  i += 1
+todel.reverse()
+for i in todel:
+  del pluginfiles[i]
+
+# try to read an object which needs a specific IO plugin
+vol = aims.read( dicomfile ) # Fails
+
+# now set back plugins
+import os
+import glob
+aimspath = glob.glob( os.path.join( aims.carto.Paths.globalShared(), 'aims-*', 'plugins', 'aimsdata.plugins' ) )[0]
+pluginfiles.append( pl.PluginFile( aimspath ) )
+pl.load()
+
+vol = aims.read( dicomfile ) # succeeds
+
