85ca1d9d70d9a526d31f4cca71ca14305f91df1b
denisri <denis.riviere.gm@gmail.com>
2010-05-04 22:33:13 +0000

    automatic conversion between Object/rc_ptr and Vertex/Edge/Graph/GraphObject: this avoids to use v.get() when using for instance a Vertex inside an Object passed to a function taking a Vertex as input.
    + Helper functions in rcptrconv.h to semi-automatize this process on other classes (TODO)
    
    
    
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/aims/aims-free/trunk@37532 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- pyaims/src/libpyaims/object/rcptrconv.h
+++ pyaims/src/libpyaims/object/rcptrconv.h
@@ -34,12 +34,38 @@
 #ifndef CARTO_PYAIMS_OBJECT_RCPTRCONV_H
 #define CARTO_PYAIMS_OBJECT_RCPTRCONV_H
 
+#include <sip.h>
 #include <cartobase/smart/sharedptr.h>
 
 namespace pyaims
 {
 
-  template <typename T> inline 
+  template <typename T> inline
+  T* fromT( PyObject * sipPy, sipWrapperType* tclass, PyObject *transferObj );
+
+  template <typename T, typename RcT> inline
+  T* fromRcptr( PyObject * sipPy, sipWrapperType* rcclass,
+                PyObject *transferObj );
+
+  template <typename T> inline
+  T* fromRcptrs( PyObject * sipPy, PyObject* tclass, PyObject* rcclass,
+                 PyObject* wsclass, PyObject* wkclass, PyObject *transferObj );
+
+  inline PyObject* extractPyObjectFromProxy( PyObject* sipPy );
+  inline int canConvertFromProxy( PyObject *sipPy, sipWrapperType* tclass );
+
+  template <typename T> inline
+  T* fromProxy( PyObject * sipPy, sipWrapperType* tclass,
+                PyObject *transferObj, int *state, int *iserr );
+
+  template <typename T> inline
+  int standardConvertToTypeCode( PyObject* sipPy, sipWrapperType* tclass,
+                                 PyObject *sipTransferObj, int *sipIsErr,
+                                 T** sipCppPtr );
+
+  // ---
+
+  template <typename T> inline
   T* fromT( PyObject * sipPy, sipWrapperType* tclass, PyObject *transferObj )
   {
     if( sipCanConvertToInstance( sipPy, tclass, SIP_NO_CONVERTORS ) )
@@ -92,6 +118,100 @@ namespace pyaims
         || fromRcptr<T, carto::weak_shared_ptr<T> >( sipPy, wsclass,
             transferObj )
         || fromRcptr<T, carto::weak_ptr<T> >( sipPy, wkclass, transferObj );
+  }
+
+
+  inline PyObject* extractPyObjectFromProxy( PyObject* sipPy )
+  {
+    if( !PyObject_HasAttrString( sipPy, "get" ) )
+      return 0;
+    PyObject* pointee = PyObject_CallMethod( sipPy, "get", NULL );
+    if( !pointee )
+    {
+      PyErr_Clear();
+      return 0;
+    }
+    return pointee;
+  }
+
+
+  inline int canConvertFromProxy( PyObject *sipPy, sipWrapperType* tclass )
+  {
+    PyObject *inside = extractPyObjectFromProxy( sipPy );
+    if( !inside )
+      return 0;
+    int res = sipCanConvertToInstance( inside, tclass, SIP_NOT_NONE );
+    Py_DECREF( inside );
+    return res;
+  }
+
+
+  template <typename T> inline
+  T* fromProxy( PyObject * sipPy, sipWrapperType* tclass,
+                PyObject *transferObj, int *state, int *iserr )
+  {
+    PyObject *inside = extractPyObjectFromProxy( sipPy );
+    if( !inside )
+      return 0;
+    int res = sipCanConvertToInstance( inside, tclass, SIP_NOT_NONE );
+    if( !res )
+    {
+      Py_DECREF( inside );
+      return 0;
+    }
+    T *sipRes = (T*) sipConvertToInstance( inside, tclass, transferObj,
+                                           SIP_NOT_NONE, state, iserr );
+    Py_DECREF( inside );
+    if( *iserr )
+    {
+      sipReleaseInstance( sipRes, tclass, *state );
+      sipRes = 0;
+    }
+    return sipRes;
+  }
+
+
+  template <typename T> inline
+  int standardConvertToTypeCode( PyObject* sipPy, sipWrapperType* tclass,
+                                 PyObject *sipTransferObj, int *sipIsErr,
+                                 T** sipCppPtr )
+  {
+    if( !sipIsErr )
+      return sipCanConvertToInstance( sipPy, tclass,
+                                      SIP_NOT_NONE | SIP_NO_CONVERTORS )
+        || pyaims::canConvertFromProxy( sipPy, tclass );
+
+    if( sipPy == Py_None )
+    {
+      *sipCppPtr = 0;
+      return 0;
+    }
+
+    int state = 0;
+
+    T * dat = (T *) sipForceConvertToInstance( sipPy, tclass, sipTransferObj,
+                                               SIP_NO_CONVERTORS, &state,
+                                               sipIsErr );
+    if( dat && *sipIsErr )
+    {
+      sipReleaseInstance( dat, tclass, state );
+      dat = 0;
+    }
+    else if( dat )
+    {
+      *sipCppPtr = dat;
+      return 0; //sipGetState(sipTransferObj);
+    }
+
+    *sipIsErr = 0;
+    *sipCppPtr = fromProxy<T>( sipPy, tclass, sipTransferObj, &state,
+                               sipIsErr );
+    if( !*sipIsErr )
+    {
+      PyErr_Clear();
+      return 0; //sipGetState(sipTransferObj);
+    }
+    return 0;
   }
 
 }
