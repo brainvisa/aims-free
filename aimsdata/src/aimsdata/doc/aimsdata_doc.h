0b8e9afb039dd0f4f49f4319ea30eac99d094e73
dgeffroy <dgeffroy@users.noreply.github.com>
2011-01-28 13:01:45 +0000

    Set list of headers as doxygen input instead of the inlcude directory to enable a dependency on the headers. #1790
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/aims/aims-free/trunk@40706 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- aimsdata/src/aimsdata/doc/aimsdata_doc.h
+++ aimsdata/src/aimsdata/doc/aimsdata_doc.h
@@ -97,7 +97,7 @@
 
   \section aimsdata_building Building:
 
-  Use cmake. More information on <a href="https://bioproj.extra.cea.fr/redmine/projects/brainvisa-devel/wiki/How_to_compile_BrainVISA_projects">this wiki page on Bioproj</a>, our Redmine based projects platform. To know more about Bioproj, see <a href="http://brainvisa.info/repository.html">this page on BrainVISA website</a>.
+  Use <a href="http://www.cmake.org/">CMake</a>, a cross-platform and open-source build system. More information on <a href="https://bioproj.extra.cea.fr/redmine/projects/brainvisa-devel/wiki/How_to_compile_BrainVISA_projects">this wiki page on Bioproj</a>, our Redmine based projects platform. To know more about Bioproj, see <a href="http://brainvisa.info/repository.html">this page on BrainVISA website</a>.
 
 
   \section aimsdata_maincontents Main contents:
