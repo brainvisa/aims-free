8cfed542a2d28ba51e2b36ef398dbc2c917f1bf9
dgeffroy <dgeffroy@users.noreply.github.com>
2011-05-05 14:50:01 +0000

    fix inheritance_diagram module path
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/aims/aims-free/branches/4.1@42675 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- pyaims/doc/sphinx/conf.py
+++ pyaims/doc/sphinx/conf.py
@@ -27,7 +27,7 @@ except Exception, e:
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.ifconfig', 'sphinx.ext.viewcode', 'sphinx.ext.extlinks', 'inheritance_diagram']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.ifconfig', 'sphinx.ext.viewcode', 'sphinx.ext.extlinks', 'sphinx.ext.inheritance_diagram']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
