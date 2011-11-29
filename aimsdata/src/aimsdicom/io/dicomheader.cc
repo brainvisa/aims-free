03d2b32cd324baa0911b0044b468911044955a56
mreynal <mreynal@bioproj.invalid>
2011-11-29 14:41:31 +0000

    merge from trunk for dicom fixes
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/aims/aims-free/branches/4.1@45807 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- aimsdata/src/aimsdicom/io/dicomheader.cc
+++ aimsdata/src/aimsdicom/io/dicomheader.cc
@@ -724,7 +724,8 @@ int DicomHeader::readFirst()
     }
 
   if ( spaceSlice > 0.0f )  voxSize.push_back( (float)spaceSlice );
-  //else if ( thickness > 0.0f )  voxSize.push_back( (float)thickness );
+  else if ( ( modality == "PT" ) && ( thickness > 0.0f ) )
+    voxSize.push_back( (float)thickness );
   else  voxSize.push_back( 0.0f ); // to determine Z resolution in read()
   voxSize.push_back( 1.0f ); // T dimension
   setProperty( "voxel_size", voxSize );
