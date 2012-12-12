cf9fa9d85cfb953ee344ed4b2fe93cea35d6ad16
denisri <denis.riviere.gm@gmail.com>
2012-12-12 17:23:40 +0000

    Fixing fix for nitfi writer when image dimensions have changed: must apply the storage_to_memory transform to the image dimensions in disk orientation, not in memory orientation. #6780
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/aims/aims-free/branches/4.3@52491 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- aimsdata/src/aimsnifti/io/niftiheader.cc
+++ aimsdata/src/aimsnifti/io/niftiheader.cc
@@ -694,17 +694,6 @@ bool NiftiHeader::fillNim( bool allow4d )
   if(getProperty( "storage_to_memory", storage_to_memory ))
   {
     s2m = storage_to_memory;
-    // fix s2m dims if needed (happens in case image dims have changed)
-    Point3df p( dims[0], dims[1], dims[2] ), pp( 0. );
-    p = s2m.transform( p ) - s2m.transform( Point3df( 0. ) );
-    if( p[0] < 0 )
-      pp[0] = -p[0] - 1;
-    if( p[1] < 0 )
-      pp[1] = -p[1] - 1;
-    if( p[2] < 0 )
-      pp[2] = -p[2] - 1;
-    s2m.setTranslation( -s2m.translation() ); // erase initial translation
-    s2m.setTranslation( pp );
   }
   else
   {
@@ -725,6 +714,18 @@ bool NiftiHeader::fillNim( bool allow4d )
   Point4d tdims = Point4d( short( rint( fabs( df[0] ) ) ),
                  short( rint( fabs( df[1] ) ) ),
                  short( rint( fabs( df[2] ) ) ), dims[3] );
+  // fix s2m dims if needed (happens in case image dims have changed)
+  s2m.setTranslation( -s2m.translation() ); // erase initial translation
+  Point3df p = s2m.transform( Point3df( tdims[0], tdims[1], tdims[2] ) ), 
+    pp( 0. );
+  if( p[0] < 0 )
+    pp[0] = -p[0] - 1;
+  if( p[1] < 0 )
+    pp[1] = -p[1] - 1;
+  if( p[2] < 0 )
+    pp[2] = -p[2] - 1;
+  s2m.setTranslation( pp );
+  
   nim->ndim = nim->dim[0] = dims.size();
   nim->dim[1] = tdims[0];
   nim->dim[2] = tdims[1];
