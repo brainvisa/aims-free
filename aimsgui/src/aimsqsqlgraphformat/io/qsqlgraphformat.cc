6c34963dd2c348b281a902c848da8b04c2af940b
denisri <denis.riviere.gm@gmail.com>
2013-11-14 12:59:02 +0000

    moved the Qt app instantiation because it was needed earlier, in plugin initialization
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/aims/aims-free/branches/4.4@59397 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- aimsgui/src/aimsqsqlgraphformat/io/qsqlgraphformat.cc
+++ aimsgui/src/aimsqsqlgraphformat/io/qsqlgraphformat.cc
@@ -47,7 +47,6 @@
 #include <qsqlquery.h>
 #include <qsqlerror.h>
 #include <qvariant.h>
-#include <QCoreApplication>
 
 using namespace aims;
 using namespace carto;
@@ -56,14 +55,6 @@ using namespace std;
 
 bool FinderQSqlGraphFormat::check( const string & filename, Finder & f ) const
 {
-  if( !QCoreApplication::instance() )
-  {
-    static int argc;
-    static char * argv[] = { (char *) "aims", (char *) 0 };
-    // WARNING: should maybe in the main thread / with a Mutex
-    new QCoreApplication( argc, argv );
-  }
-
   QSqlGraphFormatHeader *hdr = new QSqlGraphFormatHeader( filename );
   try
     {
