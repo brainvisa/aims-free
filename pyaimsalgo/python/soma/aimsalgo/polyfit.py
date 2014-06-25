932b8b29203150b014d345418bc9ed85181615ee
denisri <denis.riviere.gm@gmail.com>
2014-06-25 17:02:34 +0000

    make masks optional (non-tested)
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/aims/aims-free/trunk@62637 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- pyaimsalgo/python/soma/aimsalgo/polyfit.py
+++ pyaimsalgo/python/soma/aimsalgo/polyfit.py
@@ -21,14 +21,19 @@ def meshgrid_volume(volume):
                        #step_y * np.arange(dim_y),
                        #step_z * np.arange(dim_z), indexing="ij")
 
-def _build_fitpoly_matrix(volume, order, mask, transformation=None):
+def _build_fitpoly_matrix(volume, order, mask=None, transformation=None):
     X, Y, Z = meshgrid_volume(volume)
     if transformation is not None:
         # TODO
         raise NotImplementedError("coordinate transformation not implemented")
-    maskedX = X[mask]
-    maskedY = Y[mask]
-    maskedZ = Z[mask]
+    if mask is None:
+        maskedX = X
+        maskedX = X
+        maskedX = X
+    else:
+        maskedX = X[mask]
+        maskedY = Y[mask]
+        maskedZ = Z[mask]
     P = np.empty((maskedX.size, order ** 3))
     for k, m, n in itertools.product(range(order), repeat=3):
         P[:, k * (order ** 2) + m * order + n] = ((maskedX ** k) *
@@ -36,17 +41,20 @@ def _build_fitpoly_matrix(volume, order, mask, transformation=None):
                                                   (maskedZ ** n))
     return P
 
-def fit_poly_coefs(volume, mask, order):
+def fit_poly_coefs(volume, order, mask=None):
     """Get the coefficients of the polynomial that fits the input data"""
     P = _build_fitpoly_matrix(volume, order, mask)
     #data = np.asarray(volume)[:, :, :, 0]
     data = np.array(volume, copy=True)[:, :, :, 0]
     Q = np.linalg.pinv(P)
     #c = np.dot(Q, data[mask].ravel())
-    c = np.dot(Q, data[mask])
+    if mask is None:
+        c = np.dot(Q, data.ravel())
+    else:
+        c = np.dot(Q, data[mask])
     return c
 
-def apply_poly(volume_like, mask, coefs, transformation=None):
+def apply_poly(volume_like, coefs, mask=None, transformation=None):
     """Calculate a polynomial over the domain of the supplied mask.
 
     Return a numpy array the same size as the input mask.
@@ -56,8 +64,15 @@ def apply_poly(volume_like, mask, coefs, transformation=None):
     """
     order = int(round(len(coefs) ** (1 / 3)))
     assert order ** 3 == len(coefs)
+    volume_like_array = np.asarray(volume_like)
+    if mask is not None:
+        assert mask.shape == volume_like_array.shape[:3]
     P = _build_fitpoly_matrix(volume_like, order, mask, transformation)
-    assert mask.shape == np.asarray(volume_like).shape[:3]
-    ret = np.zeros_like(mask, dtype=float)
-    ret[mask] = np.dot(P, coefs)
+    if mask is None:
+        ret = np.dot(P, coefs)
+    else:
+        ret = np.zeros_like(
+            volume_like_array.reshape(volume_like_array.shape[:3]),
+            dtype=float)
+        ret[mask] = np.dot(P, coefs)
     return ret
