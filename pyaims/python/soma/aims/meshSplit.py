dc73727c741f682621a6a8764214a3387f8c9acf
denisri <denis.riviere.gm@gmail.com>
2014-07-29 16:03:30 +0000

    handle a time step position in texture in meshSplit
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/aims/aims-free/trunk@63150 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- pyaims/python/soma/aims/meshSplit.py
+++ pyaims/python/soma/aims/meshSplit.py
@@ -7,10 +7,10 @@
 #
 # This software is governed by the CeCILL-B license under
 # French law and abiding by the rules of distribution of free software.
-# You can  use, modify and/or redistribute the software under the 
+# You can  use, modify and/or redistribute the software under the
 # terms of the CeCILL-B license as circulated by CEA, CNRS
-# and INRIA at the following URL "http://www.cecill.info". 
-# 
+# and INRIA at the following URL "http://www.cecill.info".
+#
 # As a counterpart to the access to the source code and  rights to copy,
 # modify and redistribute granted by the license, users are provided only
 # with a limited warranty  and the software's author,  the holder of the
@@ -24,8 +24,8 @@
 # therefore means  that it is reserved for developers  and  experienced
 # professionals having in-depth computer knowledge. Users are therefore
 # encouraged to load and test the software's suitability as regards their
-# requirements in conditions enabling the security of their systems and/or 
-# data to be ensured and,  more generally, to use and operate it in the 
+# requirements in conditions enabling the security of their systems and/or
+# data to be ensured and,  more generally, to use and operate it in the
 # same conditions as regards security.
 #
 # The fact that you are presently reading this means that you have had
@@ -33,18 +33,18 @@
 import numpy
 from soma import aims
 
-def meshSplit(mesh,tex,graph):
+def meshSplit(mesh, tex, graph, tex_time_step=0):
     '''Splits a mesh into patches corresponding to a labels texture. Patches are
     organized into a graph.
-    
+
     The graph must preexist, and nodes will be inserted into it.
     '''
-    labels = numpy.unique(tex[0].arraydata())
+    labels = numpy.unique(tex[tex_time_step].arraydata())
     labels = labels.tolist()
     labels.remove(0)
     num_labels = len(labels)
     tex2 = aims.TimeTexture_S16()
-    tx = tex[0]
+    tx = tex[tex_time_step]
     tx2 = tex2[0]
     tx2.reserve(tx.size())
     for i in tx:
@@ -57,7 +57,7 @@ def meshSplit(mesh,tex,graph):
         aims.GraphManip.storeAims(graph, v._get(), 'roi_mesh', aims.rc_ptr_AimsTimeSurface_3(sub_mesh[0]))
     return graph
 
-def meshSplit2(mesh, tex, graph, voxel_size):
+def meshSplit2(mesh, tex, graph, voxel_size, tex_time_step=None):
     """
     inputs:
         - mesh : cortex mesh for example
@@ -65,18 +65,24 @@ def meshSplit2(mesh, tex, graph, voxel_size):
         - graph: :py:class:`soma.aims.Graph`, __syntax__:'roi'
 
     outputs:
-        - None: modify the input graph: add vertex : submeshes (one per texture 
+        - None: modify the input graph: add vertex : submeshes (one per texture
         label) and associated buckets add vertex "others" : void
     """
-    
+
+    # filter texture time step
+    if tex_time_step is not None and tex.size() != 0:
+        tex_tstep = aims.TimeTexture_S16()
+        tex_tstep[0] = tex[tex_time_step]
+    else:
+        tex_tstep = tex
     # labels in the texture
-    labels = (numpy.unique(tex[0].arraydata())).tolist()
+    labels = (numpy.unique(tex_tstep[0].arraydata())).tolist()
     if labels.count(0) > 1:
         labels.remove(0)
-        
+
     # 3D resolution: replace 0 by 1
     voxel_size = [1 if x == 0 else x for x in voxel_size]
-    
+
     # for each label, roi_mesh and aims_roi are added in graph
 
     for label in labels:
@@ -84,26 +90,26 @@ def meshSplit2(mesh, tex, graph, voxel_size):
         v = graph.addVertex('roi')
         v['name'] = str(label)
         v['roi_label'] = label
-        
+
         # mesh corresponding to texture of label
-        sub_mesh = aims.SurfaceManip.meshExtract(mesh, tex, label)
-        aims.GraphManip.storeAims(graph, v._get(), 'roi_mesh', aims.rc_ptr_AimsTimeSurface_3(sub_mesh[0]))
+        sub_mesh = aims.SurfaceManip.meshExtract(mesh, tex_tstep, label)
+        aims.GraphManip.storeAims(graph, v._get(), 'roi_mesh',
+            aims.rc_ptr_AimsTimeSurface_3(sub_mesh[0]))
 
         # (2) aims_roi
         bucketMap = aims.BucketMap_VOID()
         sub_mesh_vertex = sub_mesh[0].vertex()
-        
-        countVertex = 0
+
         for vertex in sub_mesh_vertex:
             px = round(vertex[0] / voxel_size[0])
             py = round(vertex[1] / voxel_size[1])
             pz = round(vertex[2] / voxel_size[2])
             p3d = aims.Point3d(px, py, pz)
             bucketMap[0][p3d] = 1
-            countVertex += 1
-        
+
         if len(voxel_size) >= 3:
             bucketMap.setSizeXYZT(voxel_size[0],voxel_size[1], voxel_size[2], 1)
         else:
             bucketMap.setSizeXYZT(1, 1, 1, 1)
         aims.GraphManip.storeAims(graph, v._get(), 'aims_roi', aims.rc_ptr_BucketMap_VOID(bucketMap))
+
