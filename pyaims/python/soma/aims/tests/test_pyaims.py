9eba4e337ed24f068f672efb1492282309489d12
denisri <denis.riviere.gm@gmail.com>
2014-08-06 09:48:53 +0000

    autopep8
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/aims/aims-free/trunk@63231 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- pyaims/python/soma/aims/tests/test_pyaims.py
+++ pyaims/python/soma/aims/tests/test_pyaims.py
@@ -2,45 +2,51 @@ import unittest
 import doctest
 import urllib
 try:
-  from soma import zipfile
+    from soma import zipfile
 except:
-  import zipfile
+    import zipfile
 import os
 from shutil import rmtree
 import tempfile
-from PyQt4.QtGui import QApplication 
+from PyQt4.QtGui import QApplication
 import sys
 
+
 def setup_doctest(test):
-  tempdir=tempfile.gettempdir()
-  tests_dir = os.path.join(tempdir, "tmp_tests_brainvisa")
-  if not os.path.exists(tests_dir):
-    os.mkdir(tests_dir)
-  os.chdir(tests_dir)
-  if not os.path.exists("demo_data.zip"):
-    print "Download ftp://ftp.cea.fr/pub/dsv/anatomist/data/demo_data.zip to ", tests_dir
-    urllib.urlretrieve("ftp://ftp.cea.fr/pub/dsv/anatomist/data/demo_data.zip", "demo_data.zip")
-  if os.path.exists("data_for_anatomist"):
-    rmtree("data_for_anatomist")
-  if os.path.exists("data_unprocessed"):
-    rmtree("data_unprocessed")
-  zf = zipfile.ZipFile("demo_data.zip")
-  zf.extractall()
-  # must create a QApplication for PyAnatomist
-  test.globs['qapp']=QApplication(sys.argv)
+    tempdir = tempfile.gettempdir()
+    tests_dir = os.path.join(tempdir, "tmp_tests_brainvisa")
+    if not os.path.exists(tests_dir):
+        os.mkdir(tests_dir)
+    os.chdir(tests_dir)
+    if not os.path.exists("demo_data.zip"):
+        print "Download ftp://ftp.cea.fr/pub/dsv/anatomist/data/demo_data.zip to ", tests_dir
+        urllib.urlretrieve(
+            "ftp://ftp.cea.fr/pub/dsv/anatomist/data/demo_data.zip",
+            "demo_data.zip")
+    if os.path.exists("data_for_anatomist"):
+        rmtree("data_for_anatomist")
+    if os.path.exists("data_unprocessed"):
+        rmtree("data_unprocessed")
+    zf = zipfile.ZipFile("demo_data.zip")
+    zf.extractall()
+    # must create a QApplication for PyAnatomist
+    test.globs['qapp'] = QApplication(sys.argv)
+
 
 def teardown_doctest(test):
-  os.chdir(test.globs['curdir'])
-  
+    os.chdir(test.globs['curdir'])
+
+
 def test_suite():
-  suite = unittest.TestSuite()
-  doctest_suite = unittest.TestSuite(doctest.DocFileSuite("pyaims_tutorial.rst", 
-                                                          globs={'curdir' : os.path.abspath(os.curdir)}, 
-                                                          setUp=setup_doctest, 
-                                                          tearDown=teardown_doctest, 
-                                                          optionflags=doctest.ELLIPSIS))
-  suite.addTest(doctest_suite)
-  return suite
+    suite = unittest.TestSuite()
+    doctest_suite = unittest.TestSuite(
+        doctest.DocFileSuite("pyaims_tutorial.rst",
+                             globs={'curdir': os.path.abspath(os.curdir)},
+                             setUp=setup_doctest,
+                             tearDown=teardown_doctest,
+                             optionflags=doctest.ELLIPSIS))
+    suite.addTest(doctest_suite)
+    return suite
 
 if __name__ == '__main__':
     unittest.main(defaultTest='test_suite')
