22ba7e406bc6f67edebc5c55e16769bd1ecf3774
appli <appli@bioproj.invalid>
2014-11-27 11:35:54 +0000

    Fixes a remaining syntax error
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/aims/aims-free/trunk@64403 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/aims/tests/api.py
+++ python/aims/tests/api.py
@@ -492,8 +492,9 @@ class CommandsTestManager(unittest.TestCase):
                 run_files = self.get_run_files(c)
 
                 for ref_file, run_file in zip(ref_files, run_files):
-                    sys.stderr.write('compare files: %s / %s\n' % (ref,file, run_file)
+                    sys.stderr.write('compare files: %s <=> %s\n' % (ref_file, run_file))
                     FileComparison.assertEqual(self, run_file, ref_file)
+                    
             except Exception, e:
                 print 'Test failure:', e, 'while running:', c
                 n_failed += 1
