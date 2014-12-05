e021acee193195a2e8961b4a0442aea4fe5c835d
denisri <denis.riviere.gm@gmail.com>
2014-12-05 02:11:09 +0000

    function to initialize/reuse/delete/reinstantiate Q[Core]Application after a QCoreApplication may have been instantiated. #8874
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/aims/aims-free/branches/4.4@64669 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ aimsgui/src/lib/qtmisc/initqapplication.h
@@ -0,0 +1,52 @@
+/* This software and supporting documentation are distributed by
+ *     Institut Federatif de Recherche 49
+ *     CEA/NeuroSpin, Batiment 145,
+ *     91191 Gif-sur-Yvette cedex
+ *     France
+ *
+ * This software is governed by the CeCILL-B license under
+ * French law and abiding by the rules of distribution of free software.
+ * You can  use, modify and/or redistribute the software under the
+ * terms of the CeCILL-B license as circulated by CEA, CNRS
+ * and INRIA at the following URL "http://www.cecill.info".
+ *
+ * As a counterpart to the access to the source code and  rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty  and the software's author,  the holder of the
+ * economic rights,  and the successive licensors  have only  limited
+ * liability.
+ *
+ * In this respect, the user's attention is drawn to the risks associated
+ * with loading,  using,  modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean  that it is complicated to manipulate,  and  that  also
+ * therefore means  that it is reserved for developers  and  experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systems and/or
+ * data to be ensured and,  more generally, to use and operate it in the
+ * same conditions as regards security.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL-B license and that you accept its terms.
+ */
+
+#ifndef AIMSGUI_QTMISC_INITQAPPLICATION_H
+#define AIMSGUI_QTMISC_INITQAPPLICATION_H
+
+#include <aims/config/aimsdata_config.h>
+#include <QApplication>
+
+namespace aims
+{
+
+  /** Instantiate, reuse, or replace the QApplication or QCoreApplication
+      as requested, so that GUI can take place even after a QCoreApplication
+      has been used. */
+  QCoreApplication* initializeQApplication( int argc, char** argv,
+                                            bool use_gui=true );
+
+}
+
+
+#endif
