8847c394ffff842dcff9341dececfb3c70f284f2
denisri <denis.riviere.gm@gmail.com>
2014-12-11 09:50:54 +0000

    custom CSS to make classes appear a little bit more clearly in docs
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/aims/aims-free/branches/4.4@64804 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- pyaims/doc/sphinx/conf.py
+++ pyaims/doc/sphinx/conf.py
@@ -132,6 +132,8 @@ html_theme_options = {  }
 # of the sidebar.
 #html_logo = '../../../../axon/trunk/share/icons/brainvisa.png'
 
+html_style = 'custom.css'
+
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
