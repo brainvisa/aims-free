174bf3ab3bda42f71f26fa2cd443d978eb09cc96
denisri <denis.riviere.gm@gmail.com>
2015-07-07 09:41:04 +0000

    move AimsTextureVoronoi to getopt2
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/aims/aims-free/trunk@68016 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- aimsalgo/src/AimsTextureVoronoi/main.cc
+++ aimsalgo/src/AimsTextureVoronoi/main.cc
@@ -34,7 +34,7 @@
 
 #include <aims/distancemap/meshvoronoi.h>
 #include <aims/io/io_g.h>
-#include <aims/getopt/getopt.h>
+#include <aims/getopt/getopt2.h>
 #include <aims/mesh/texture.h>
 #include <float.h>
 #include <assert.h>
@@ -48,101 +48,91 @@ using namespace std;
  
 typedef float float3[3];
 
-BEGIN_USAGE(usage)
-  "-------------------------------------------------------------------------",
-  "AimsTextureVoronoi   -m[esh] <input_mesh>                                ",
-  "                     -t[exture] <input_texture>                          ",
-  "                     -o[utput] <output_vor_texture>                      ",
-  "                     [-s[tep] <step>]                                    ",
-  "                     [-b[ackground] <background_value>]                  ",
-  "                     [-f[orbidden] <forbidden_value>]                    ",
-  "                     [-T[ime]   <time serie> default = 0]                ",
-  "                     [--c[onnexity] ]                                    ",
-  "                     [-h[elp]]                                           ",
-  "-------------------------------------------------------------------------",
-  " Compute the Voronoi diagram of the input (short int) seed texture       ",
-  "-------------------------------------------------------------------------",
-  "     input_mesh          : input *.tri or *.mesh file                    ",
-  "     input_texture       : definition of the seed (short int)",
-  "     background          : background value <default = 0>",
-  "     forbidden           : value out of the domain <default = -1>",
-  "     size                : step size <default = infinity>",
-  "     connexity           : connexity or geodesic euclidean distance      ",
-  "     output_vor_texture : output voronoi diagram texture   ",
-  "-------------------------------------------------------------------------",
-END_USAGE
 
-  
-//
-// Usage
-//
-void Usage( void )
+int main( int argc, const char** argv )
 {
-  AimsUsage( usage );
-}
-
-
-int main( int argc, char** argv )
-{
-  char	 *meshfile = 0, *intexfile = 0, *outtexfile = 0;
   float dist = FLT_MAX;
   bool connexity = false;
   short background = 0 ;
   short forbidden = -1;
   int T = 0;
-  //
-  // Parser of options
-  //
-  AimsOption opt[] = {
-    { 'h',"help"         ,AIMS_OPT_FLAG  ,( void* )Usage           ,AIMS_OPT_CALLFUNC,0},
-    { 'm',"mesh"         ,AIMS_OPT_STRING,&meshfile       ,0                ,1},
-    { 'o',"output"       ,AIMS_OPT_STRING,&outtexfile     ,0                ,1},
-    { 't',"texture"      ,AIMS_OPT_STRING,&intexfile      ,0                ,1},
-    { 'T',"Time"         ,AIMS_OPT_INT   ,&T              ,0                ,0},
-    { 'b',"background"   ,AIMS_OPT_INT   ,&background     ,0                ,0},
-    { 'f',"forbidden"    ,AIMS_OPT_INT   ,&forbidden      ,0                ,0},
-    { 's',"step"         ,AIMS_OPT_FLOAT ,&dist           ,0                ,0},
-    { ' ',"connexity"    ,AIMS_OPT_FLAG  ,&connexity      ,0                ,0},    
-    { 0  ,0              ,AIMS_OPT_END   ,0               ,0                ,0}};
+  Reader<AimsSurfaceTriangle> triR;
+  Writer<TimeTexture<short> > texW;
+  Reader<TimeTexture<short> > texR;
 
-  AimsParseOptions( &argc, argv, opt, usage );
-  
-  //
-  // read triangulation
-  //
-  cout << "reading white triangulation   : " << flush;
-  AimsSurfaceTriangle surface;
-  Reader<AimsSurfaceTriangle> triR( meshfile );
-  triR >> surface;
-  cout << "done" << endl;
-  
-  //
-  // read input texture
-  //
-  cout << "reading texture   : " << flush;
-  TimeTexture<short>	seed, voronoi; 
-  Reader<TimeTexture<short> > texR( intexfile );
-  texR >> seed; 
-  cout << "done" << endl;
-  
-  cout << "mesh vertices : " << surface[0].vertex().size() << endl;
-  cout << "mesh polygons : " << surface[0].polygon().size() << endl;
-  cout << "Object texture dim   : " << seed[0].nItem() << endl;
-  cout << "Background label: " << background << " Forbidden label : " << forbidden << endl;
-  if ( ! (  seed[0].nItem() ==  surface[0].vertex().size() ) )
-    {
-      cerr << "The triangulation and the textures must correspond to the same object \n";
-      assert( 0 );
-    }
-  
-  cout << "Compute the voronoi diagram\n";
-  voronoi[0] = MeshVoronoiT<short>( surface[0], seed[(unsigned) T], background ,forbidden, dist ,connexity,true );
-  cout << "writing texture : " << flush;
-  Writer<TimeTexture<short> >	texW( outtexfile );
-  texW <<  voronoi ;
-  cout << "done" << endl;
+  AimsApplication app( argc, argv,
+                       "Compute the Voronoi diagram of the input (short int) "
+                       "seed texture" );
+  app.addOption( triR, "-m", "input_mesh" );
+  app.addOption( texW, "-o", "output voronoi diagram texture" );
+  app.addOption( texR, "-t", "seeds texture definition (short int)" );
+  app.addOption( T, "-T", "time step <default = 0>", true );
+  app.addOption( background, "-b", "background value <default = 0>", true );
+  app.addOption( forbidden, "-f", "value out of the domain <default = -1>",
+                 true );
+  app.addOption( dist, "-s", "step size <default = infinity>", true );
+  app.addOption( connexity, "-c",
+                 "connectivity or geodesic euclidean distance "
+                 "<default: euclidean>", true );
+  app.alias( "--mesh", "-m" );
+  app.alias( "--output", "-o" );
+  app.alias( "--texture", "-t" );
+  app.alias( "--Time", "-T" );
+  app.alias( "--background", "-b" );
+  app.alias( "--forbidden", "-f" );
+  app.alias( "--step", "-s" );
+  app.alias( "--connexity", "-c" );
+  app.alias( "--connectivity", "-c" );
 
-  return( 0 );  
+  try
+  {
+    app.initialize();
+
+    //
+    // read triangulation
+    //
+    cout << "reading white triangulation   : " << flush;
+    AimsSurfaceTriangle surface;
+    triR >> surface;
+    cout << "done" << endl;
+
+    //
+    // read input texture
+    //
+    cout << "reading texture   : " << flush;
+    TimeTexture<short>    seed, voronoi;
+    texR >> seed;
+    cout << "done" << endl;
+
+    cout << "mesh vertices : " << surface[0].vertex().size() << endl;
+    cout << "mesh polygons : " << surface[0].polygon().size() << endl;
+    cout << "Object texture dim   : " << seed[0].nItem() << endl;
+    cout << "Background label: " << background << " Forbidden label : "
+      << forbidden << endl;
+    if ( ! (  seed[0].nItem() ==  surface[0].vertex().size() ) )
+      {
+        cerr << "The triangulation and the textures must correspond to the same object \n";
+        assert( 0 );
+      }
+
+    cout << "Compute the voronoi diagram\n";
+    voronoi[0] = MeshVoronoiT<short>( surface[0], seed[(unsigned) T],
+                                      background ,forbidden, dist,
+                                      connexity, true );
+    cout << "writing texture : " << flush;
+    texW <<  voronoi ;
+    cout << "done" << endl;
+
+    return EXIT_SUCCESS;
+  }
+  catch( user_interruption & )
+  {
+  }
+  catch( exception & e )
+  {
+    cerr << e.what() << endl;
+  }
+
+  return EXIT_FAILURE;
 }
 
- 
