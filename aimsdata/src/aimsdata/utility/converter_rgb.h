70e4510f9a465da7c849b212b01de3445771672a
balbasty <balbasty@users.noreply.github.com>
2015-11-09 16:13:44 +0000

    Add AimsRGB <-> AimsVector converters
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/aims/aims-free/trunk@70054 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- aimsdata/src/aimsdata/utility/converter_rgb.h
+++ aimsdata/src/aimsdata/utility/converter_rgb.h
@@ -48,7 +48,7 @@ namespace carto
   };
 
   template<class INP>
-  inline void 
+  inline void
   RawConverter<INP,AimsRGB>::convert( const INP &in, AimsRGB & out ) const
   {
     out = AimsRGB( (byte) in, (byte) in, (byte) in );
@@ -62,7 +62,7 @@ namespace carto
   };
 
   template<class INP>
-  inline void 
+  inline void
   RawConverter<INP,AimsRGBA>::convert( const INP &in, AimsRGBA & out ) const
   {
     out = AimsRGBA( (byte) in, (byte) in, (byte) in, 255 );
@@ -76,11 +76,11 @@ namespace carto
   };
 
   template<class OUTP>
-  inline void 
+  inline void
   RawConverter<AimsRGB,OUTP>::convert( const AimsRGB &in, OUTP & out ) const
   {
-    out = (OUTP) ( sqrt( ( (double) in.red() ) * in.red() 
-			+ ( (double) in.green() ) * in.green() 
+    out = (OUTP) ( sqrt( ( (double) in.red() ) * in.red()
+			+ ( (double) in.green() ) * in.green()
 			+ ( (double) in.blue() ) * in.blue() ) );
   }
 
@@ -92,11 +92,11 @@ namespace carto
   };
 
   template<class OUTP>
-  inline void 
+  inline void
   RawConverter<AimsRGBA,OUTP>::convert( const AimsRGBA &in, OUTP & out ) const
   {
-    out = (OUTP) ( sqrt( ( (double) in.red() ) * in.red() 
-			+ ( (double) in.green() ) * in.green() 
+    out = (OUTP) ( sqrt( ( (double) in.red() ) * in.red()
+			+ ( (double) in.green() ) * in.green()
 			+ ( (double) in.blue() ) * in.blue() ) );
   }
 
@@ -120,6 +120,318 @@ namespace carto
       out = in;
     }
   };
+
+}
+
+//--- Vector/RGB conversion --------------------------------------------------
+#include <aims/vector/vector.h>
+#include <limits>
+
+namespace carto {
+
+  template <typename T, int D>
+  class RawConverter<AimsVector<T,D>, AimsRGB>
+  {
+  public:
+    void convert( const AimsVector<T,D> &in, AimsRGB &out ) const
+    {
+      for( uint8_t i=0; i<3 && i<in.size(); ++i )
+        out[i] = in[i];
+    }
+  };
+
+
+  template <typename T, int D>
+  class RawConverter<AimsRGB, AimsVector<T,D> >
+  {
+  public:
+    void convert( const AimsRGB &in, AimsVector<T,D> &out ) const
+    {
+      for( uint8_t i=0; i<3 && i<out.size(); ++i )
+        out[i] = in[i];
+    }
+  };
+
+
+  template <typename T, int D>
+  class RawConverter<AimsVector<T,D>, AimsRGBA>
+  {
+  public:
+    void convert( const AimsVector<T,D> &in, AimsRGBA &out ) const
+    {
+      for( uint8_t i=0; i<4 && i<in.size(); ++i )
+        out[i] = in[i];
+    }
+  };
+
+
+  template <typename T, int D>
+  class RawConverter<AimsRGBA, AimsVector<T,D> >
+  {
+  public:
+    void convert( const AimsRGBA &in, AimsVector<T,D> &out ) const
+    {
+      for( uint8_t i=0; i<4 && i<out.size(); ++i )
+        out[i] = in[i];
+    }
+  };
+
+
+//--- Vector/RGB rescaler ----------------------------------------------------
+
+  template <typename T, int D>
+  class Rescaler<AimsData<AimsVector<T,D> >, AimsData<AimsRGB> >
+  {
+  public:
+    Rescaler();
+    Rescaler( const RescalerInfo & info );
+
+    void convert( const AimsData<AimsVector<T,D> > &in, AimsData<AimsRGB> & out ) const;
+
+  private:
+    RescalerInfo _info;
+  };
+
+  template <typename T, int D>
+  inline
+  Rescaler<AimsData<AimsVector<T,D> >, AimsData<AimsRGB> >::Rescaler():
+    _info()
+  {}
+
+  template <typename T, int D>
+  inline
+  Rescaler<AimsData<AimsVector<T,D> >, AimsData<AimsRGB> >::Rescaler(
+    const RescalerInfo & info ):
+    _info(info)
+  {}
+
+  template <typename T, int D>
+  void
+  Rescaler<AimsData<AimsVector<T,D> >, AimsData<AimsRGB> >::convert(
+    const AimsData<AimsVector<T,D> > &in, AimsData<AimsRGB> &out ) const
+  {
+    if( out.dimX() == 0 )
+      out = AimsData<AimsRGB>( in.dimX(), in.dimY(), in.dimZ(),
+                               in.dimT(), in.borderWidth() );
+
+    out.setSizeX( in.sizeX() );
+    out.setSizeY( in.sizeY() );
+    out.setSizeZ( in.sizeZ() );
+    out.setSizeT( in.sizeT() );
+
+    if( in.header() )
+      out.setHeader( in.header()->cloneHeader( true ) );
+
+    RescalerInfo info( _info );
+    if( std::isnan( info.vmin ) ) {
+      if ( ! info.usevtypelimits ) {
+        info.vmin = std::numeric_limits<double>::max();
+        for( int k=0; k<in.dimZ(); ++k )
+          for( int j=0; j<in.dimY(); ++j )
+            for( int i=0; i<in.dimX(); ++i )
+            {
+              for( int c = 0; c < in(i, j, k).size(); ++c )
+              if( (double) in(i, j, k)[c] < info.vmin )
+                info.vmin = (double) in(i, j, k)[c];
+            }
+      }
+    }
+
+    if( std::isnan( info.vmax ) ) {
+      if ( ! info.usevtypelimits ) {
+        info.vmax = std::numeric_limits<double>::min();
+        for( int k=0; k<in.dimZ(); ++k )
+          for( int j=0; j<in.dimY(); ++j )
+            for( int i=0; i<in.dimX(); ++i )
+            {
+              for( int c = 0; c < in(i, j, k).size(); ++c )
+              if( (double) in(i, j, k)[c] > info.vmax )
+                info.vmax = (double) in(i, j, k)[c];
+            }
+      }
+    }
+
+    DefaultedRescalerInfo<T, uint8_t> defaultedinfo( info );
+
+    int x, y, z, t, dx = out.dimX(), dy = out.dimY(), dz = out.dimZ(),
+      dt = out.dimT(), ox = dx, oy = dy, oz = dz, ot = dt;
+    if( in.dimX() < dx )
+      dx = in.dimX();
+    if( in.dimY() < dy )
+      dy = in.dimY();
+    if( in.dimZ() < dz )
+      dz = in.dimZ();
+    if( in.dimT() < dt )
+      dt = in.dimT();
+
+    for( t=0; t<dt; ++t )
+      {
+        for( z=0; z<dz; ++z )
+          {
+            for( y=0; y<dy; ++y )
+              {
+                for( x=0; x<dx; ++x )
+                  for( int c=0; c < in( x, y, z, t ).size() && c<3; ++c )
+                    out( x, y, z, t )[c] =
+                     defaultedinfo.getScaledValue( in( x, y, z, t )[c] );
+                for( ; x<ox; ++x )
+                  out( x, y, z, t ) = AimsRGB(0);
+              }
+            for( ; y<oy; ++y )
+              for( x=0; x<ox; ++x )
+                out( x, y, z, t ) = AimsRGB(0);
+          }
+        for( ; z<oz; ++z )
+          for( y=0; y<oy; ++y )
+            for( x=0; x<ox; ++x )
+              out( x, y, z, t ) = AimsRGB(0);
+      }
+
+    for( ; t<ot; ++t )
+      for( z=0; z<oz; ++z )
+        for( y=0; y<oy; ++y )
+          for( x=0; x<ox; ++x )
+            out( x, y, z, t ) = AimsRGB(0);
+
+    float scf = 1.;
+    aims::PythonHeader
+      *h = dynamic_cast<aims::PythonHeader *>( out.header() );
+    if( !h )
+      h = new aims::PythonHeader;
+    h->getProperty( "scale_factor", scf );
+    scf *= defaultedinfo.getScale();
+    h->setProperty( "scale_factor", scf );
+    h->setProperty( "data_type", DataTypeCode<AimsRGB>::dataType() );
+    if( h->hasProperty( "disk_data_type" ) )
+      h->removeProperty( "disk_data_type" );
+  }
+
+  template <typename T, int D>
+  class Rescaler<AimsData<AimsRGB>, AimsData<AimsVector<T,D> > >
+  {
+  public:
+    Rescaler();
+    Rescaler( const RescalerInfo & info );
+
+    void convert( const AimsData<AimsRGB> &in, AimsData<AimsVector<T,D> > & out ) const;
+
+  private:
+    RescalerInfo _info;
+  };
+
+  template <typename T, int D>
+  inline
+  Rescaler<AimsData<AimsRGB>, AimsData<AimsVector<T,D> > >::Rescaler():
+    _info()
+  {}
+
+  template <typename T, int D>
+  inline
+  Rescaler<AimsData<AimsRGB>, AimsData<AimsVector<T,D> > >::Rescaler(
+    const RescalerInfo & info ):
+    _info(info)
+  {}
+
+  template <typename T, int D>
+  void
+  Rescaler<AimsData<AimsRGB>, AimsData<AimsVector<T,D> > >::convert(
+    const AimsData<AimsRGB> &in, AimsData<AimsVector<T,D> > &out ) const
+  {
+    if( out.dimX() == 0 )
+      out = AimsData<AimsVector<T,D> >( in.dimX(), in.dimY(), in.dimZ(),
+                                        in.dimT(), in.borderWidth() );
+
+    out.setSizeX( in.sizeX() );
+    out.setSizeY( in.sizeY() );
+    out.setSizeZ( in.sizeZ() );
+    out.setSizeT( in.sizeT() );
+
+    if( in.header() )
+      out.setHeader( in.header()->cloneHeader( true ) );
+
+    RescalerInfo info( _info );
+    if( std::isnan( info.vmin ) ) {
+      if ( ! info.usevtypelimits ) {
+        info.vmin = std::numeric_limits<double>::max();
+        for( int k=0; k<in.dimZ(); ++k )
+          for( int j=0; j<in.dimY(); ++j )
+            for( int i=0; i<in.dimX(); ++i )
+            {
+              for( int c = 0; c < 3; ++c )
+              if( (double) in(i, j, k)[c] < info.vmin )
+                info.vmin = (double) in(i, j, k)[c];
+            }
+      }
+    }
+
+    if( std::isnan( info.vmax ) ) {
+      if ( ! info.usevtypelimits ) {
+        info.vmax = std::numeric_limits<double>::min();
+        for( int k=0; k<in.dimZ(); ++k )
+          for( int j=0; j<in.dimY(); ++j )
+            for( int i=0; i<in.dimX(); ++i )
+            {
+              for( int c = 0; c < 3; ++c )
+              if( (double) in(i, j, k)[c] > info.vmax )
+                info.vmax = (double) in(i, j, k)[c];
+            }
+      }
+    }
+
+    DefaultedRescalerInfo<uint8_t, T> defaultedinfo( info );
+
+    int x, y, z, t, dx = out.dimX(), dy = out.dimY(), dz = out.dimZ(),
+      dt = out.dimT(), ox = dx, oy = dy, oz = dz, ot = dt;
+    if( in.dimX() < dx )
+      dx = in.dimX();
+    if( in.dimY() < dy )
+      dy = in.dimY();
+    if( in.dimZ() < dz )
+      dz = in.dimZ();
+    if( in.dimT() < dt )
+      dt = in.dimT();
+
+    for( t=0; t<dt; ++t )
+      {
+        for( z=0; z<dz; ++z )
+          {
+            for( y=0; y<dy; ++y )
+              {
+                for( x=0; x<dx; ++x )
+                  for( int c=0; c < out( x, y, z, t ).size() && c<3; ++c )
+                    out( x, y, z, t )[c] =
+                     defaultedinfo.getScaledValue( in( x, y, z, t )[c] );
+                for( ; x<ox; ++x )
+                  out( x, y, z, t ) = AimsRGB(0);
+              }
+            for( ; y<oy; ++y )
+              for( x=0; x<ox; ++x )
+                out( x, y, z, t ) = AimsRGB(0);
+          }
+        for( ; z<oz; ++z )
+          for( y=0; y<oy; ++y )
+            for( x=0; x<ox; ++x )
+              out( x, y, z, t ) = AimsRGB(0);
+      }
+
+    for( ; t<ot; ++t )
+      for( z=0; z<oz; ++z )
+        for( y=0; y<oy; ++y )
+          for( x=0; x<ox; ++x )
+            out( x, y, z, t ) = AimsRGB(0);
+
+    float scf = 1.;
+    aims::PythonHeader
+      *h = dynamic_cast<aims::PythonHeader *>( out.header() );
+    if( !h )
+      h = new aims::PythonHeader;
+    h->getProperty( "scale_factor", scf );
+    scf *= defaultedinfo.getScale();
+    h->setProperty( "scale_factor", scf );
+    h->setProperty( "data_type", DataTypeCode<AimsRGB>::dataType() );
+    if( h->hasProperty( "disk_data_type" ) )
+      h->removeProperty( "disk_data_type" );
+  }
 
 }
 
