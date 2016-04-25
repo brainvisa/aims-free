58a4674f2d3261bedc742b748149c75f574996af
balbasty <balbasty@users.noreply.github.com>
2016-04-25 10:52:50 +0000

    refactor splines: add several implementations (recursive, fast, tabulated, discrete)
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/aims/aims-free/trunk@72791 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- aimsalgo/src/public/math/bspline.h
+++ aimsalgo/src/public/math/bspline.h
@@ -34,6 +34,7 @@
 #ifndef AIMSALGO_MATH_BSPLINE_H
 #define AIMSALGO_MATH_BSPLINE_H
 
+#include <aims/vector/vector.h>         // Point2dd
 #include <cartobase/exception/assert.h>
 #include <cmath>    // std::abs
 #include <limits>   // std::numeric_limits
@@ -51,6 +52,11 @@ namespace aims {
   template <unsigned Order>
   struct BSplineOrder
   {
+    static double at( double x )
+    {
+      return spline(x);
+    }
+
     static double spline( double x )
     {
       if( std::abs(x) > double(Order+1)/2 )
@@ -102,25 +108,132 @@ namespace aims {
   //
   //==========================================================================
 
-  /// B-Spline function centered on 0, evaluated at run time.
+  /// *Centered and *scaled B-Spline function, evaluated at run time.
+  ///
+  /// Centered B-splines of order \c n are recursively evaluated following the
+  /// formula:
+  /// \f[
+  ///   \Beta^n(x) = \frac{
+  ///     (\frac{n+1}{2} + x) \Beta^{n-1}( x + \frac{1}{2} ) +
+  ///     (\frac{n+1}{2} - x) \Beta^{n-1}( x - \frac{1}{2} )
+  ///   }{n}
+  /// \f]
+  ///
+  /// Let \c m be the scaling factor, the scaled B-Spline is then equal to
+  /// \f$b_m^n(x) = \Beta^n( \frac{x}{m} )\f$
+  ///
+  /// A shifted scaled B-Spline is equal to:
+  /// \f$c_m^n(x) = \Beta^n( \frac{x}{m} + \frac{1}{2} )\f$
+  ///
+  /// See Unser, Aldroubi & Eden: "B-Spline Signal Processing: Part I -
+  /// Theory" in IEEE Transactions on Signal Processing (February 1993).
+  ///
+  /// In this implementation, unshifted centered B-Spline are recursively
+  /// evaluated at value x/m (+ .5).
+  ///
+  /// \note This is also a base class for particular spline implementations
+  /// ( fast, tabular, ... ). Methods are virtual so this class can be used
+  /// in polymorphic contexts
   class BSpline
   {
   public:
-    BSpline( unsigned order = 3 );
-    ~BSpline();
+    BSpline( unsigned order = 3, float scale = 1., bool shifted = false );
+    BSpline( const BSpline & other );
+    virtual ~BSpline();
+    BSpline & operator= ( const BSpline & other );
 
+    /// Compute value
+    /// @{
     /// \see spline()
+    virtual
     double   operator() ( double x )                const;
     /// spline value at point x
-    double   spline( double x )                     const;
+    virtual
+    double   at( double x )                     const;
     /// n-th derivative of the spline at point x
+    virtual
     double   derivative( double x, unsigned n = 1 ) const;
-    /// spline order
-    unsigned order()                                const;
+    /// Get the inferior and superior boundaries of the function support
+    /// (i.e. the interval on which it is not null).
+    /// If the order is not zero, boundaries are not included in the support.
+    /// Else, they are.
+    virtual
+    const Point2dd & support() const;
+    /// @}
 
-  private:
+    /// Set parameters
+    /// @ {
+    /// (Re)set all parameters
+    virtual
+    void reset( unsigned order = 3, float scale = 1., bool shifted = false );
+    /// order
+    virtual
+    void setOrder( unsigned order );
+    /// scale
+    virtual
+    void setScale( float scale );
+    /// shifted
+    virtual
+    void setShifted( bool shifted );
+    /// centered
+    virtual
+    void setCentered( bool centered );
+    /// @}
+
+    /// Get parameters
+    /// @ {
+    /// spline order
+    virtual
+    unsigned order()    const;
+    /// spline scale
+    virtual
+    float    scale()    const;
+    /// is shifted ?
+    virtual
+    bool     shifted()  const;
+    /// is centered ?
+    virtual
+    bool     centered() const;
+    /// @}
+
+  protected:
     double dO()     const;
+    virtual void setSupport();
     unsigned _order;
+    float    _scale;
+    bool     _shift;
+    Point2dd _support;
+  };
+
+  //==========================================================================
+  //
+  //     B-SPLINE : FAST
+  //
+  //==========================================================================
+
+  /// *Centered and *scaled B-Spline function, evaluated at run time.
+  ///
+  /// Here, analytical expression are hard coded for orders 0 to 7.
+  ///
+  /// In this implementation, unshifted centered B-Spline are
+  /// evaluated at value x/m (+ .5).
+  class FastBSpline: public BSpline
+  {
+  public:
+    FastBSpline( unsigned order = 3, float scale = 1., bool shifted = false );
+    FastBSpline( const FastBSpline & other );
+    virtual ~FastBSpline();
+    FastBSpline & operator= ( const FastBSpline & other );
+
+    /// Compute value
+    /// @{
+    /// spline value at point x
+    double   at( double x )                     const;
+    /// n-th derivative of the spline at point x
+    /// \warning DO NOT USE: not impleùented yet
+    double   derivative( double x, unsigned n = 1 ) const;
+    /// @}
+
   };
 
   //==========================================================================
@@ -132,30 +245,112 @@ namespace aims {
   /// Pre-computed B-Spline values
   /// In the "order 0" case, the array is not used (the analytical expression
   /// is too simple)
-  class TabulBSpline
+  ///
+  /// See BSpline for information on values computation
+  class TabulBSpline: public BSpline
   {
   public:
-    TabulBSpline( unsigned order = 3, unsigned nder = 1, unsigned length = 65537 );
-    ~TabulBSpline();
+    TabulBSpline( unsigned order = 3,
+                  unsigned nder = 1,
+                  float scale = 1.,
+                  bool shifted = false,
+                  size_t length = std::numeric_limits<unsigned short>::max() - 1 );
+    TabulBSpline( const TabulBSpline & other );
+    virtual ~TabulBSpline();
+    TabulBSpline & operator= ( const TabulBSpline & other );
 
-    /// \see spline()
-    double   operator() ( double x )                const;
+    /// Get value
+    /// @{
     /// spline value at point x
-    double   spline( double x )                     const;
+    double   at( double x )                     const;
     /// n-th derivative of the spline at point x
     double   derivative( double x, unsigned n = 1 ) const;
-    /// spline order
-    unsigned order()                                const;
-    /// number of derivative stored
-    unsigned nder()                                 const;
-    /// length
-    unsigned length()                               const;
+    /// @}
 
-  private:
-    unsigned index( double x )          const;
-    bool     is_valid( unsigned index ) const;
-    unsigned _order;
+    /// Set parameters
+    /// Be aware that a call to reset, setOrder, setNbDer and setLength
+    /// will trigger a reallocation of the array and a recomputing of
+    /// the values. If several of these values need changing, it is thus
+    /// avdised to use the reset() method.
+    ///
+    /// @{
+    /// (Re)set all parameters
+    void reset( unsigned order = 3,
+                unsigned nder = 1,
+                float scale = 1.,
+                bool shifted = false,
+                size_t length = std::numeric_limits<unsigned short>::max() - 1 );
+    /// order
+    void setOrder( unsigned order );
+    /// number of derivative stored
+    void setNbDer( unsigned nder );
+    /// array length
+    void setLength( size_t length );
+    /// @}
+
+    /// Get parameters
+    /// @{
+    /// number of derivative stored
+    unsigned nder()     const;
+    /// array length
+    size_t   length()   const;
+    /// @}
+
+  protected:
+    size_t   index( double x )          const;
+    bool     is_valid( size_t index ) const;
+    void     setArray( unsigned nder, size_t length );
     std::vector<std::vector<double> > _values;
+  };
+
+  //==========================================================================
+  //
+  //     DISCRETE B-SPLINE
+  //
+  //==========================================================================
+
+  /// Discrete B-Splines
+  /// B-Spline pre sampled only on integral values.
+  class DiscreteBSpline: public BSpline
+  {
+  public:
+    /// Constructor/Destructor/Copy
+    /// @{
+    DiscreteBSpline( unsigned order = 3, float scale = 1., bool shifted = false );
+    DiscreteBSpline( const DiscreteBSpline & other );
+    virtual ~DiscreteBSpline();
+    DiscreteBSpline & operator= ( const DiscreteBSpline & other );
+    /// @}
+
+    /// Get value
+    /// @{
+    double operator() ( int x ) const;
+    double at( int x ) const;
+    /// @}
+
+    /// Set parameters
+    /// Be aware that a call to reset, setOrder, setNbDer and setLength
+    /// will trigger a reallocation of the array and a recomputing of
+    /// the values. If several of these values need changing, it is thus
+    /// avdised to use the reset() method.
+    ///
+    /// @{
+    /// (Re)set all parameters
+    void reset( unsigned order = 3, float scale = 1., bool shifted = false );
+    /// order
+    void setOrder( unsigned order );
+    /// scale
+    void setScale( float scale );
+    /// shifted
+    void setShifted( bool shifted = true );
+    /// centered
+    void setCentered( bool centered = true );
+    /// @}
+
+  protected:
+    void setArray();
+    void setSupport();
+    std::vector<double> _values;
   };
 
 } // namespace aims
