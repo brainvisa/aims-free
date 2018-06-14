54461ded38cb12444b433f3e6e958b64716c59f3
ylep <yann.leprince@cea.fr>
2018-06-14 10:38:40 +0000

    fix harmless compiler warnings
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/aims/aims-free/trunk@81940 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- aimsalgo/src/aimsalgo/registration/ffd.cc
+++ aimsalgo/src/aimsalgo/registration/ffd.cc
@@ -846,8 +846,8 @@ SplineFfdResampler<T, C>::SplineFfdResampler(
     const FfdTransformation & transformation, T background ):
   CubicResampler<C>(),
   _transformation(transformation),
-  _background(background),
-  _spline(3, 0)
+  _spline(3, 0),
+  _background(background)
 {
   init();
 }
@@ -857,10 +857,10 @@ SplineFfdResampler<T, C>::SplineFfdResampler(
     const FfdTransformation & transformation, const AffineTransformation3d & affine,
     T background ):
   CubicResampler<C>(),
-  _transformation(transformation),
   _affine(affine),
-  _background(background),
-  _spline(3, 0)
+  _transformation(transformation),
+  _spline(3, 0),
+  _background(background)
 {
   init();
 }
@@ -909,7 +909,6 @@ Point3df SplineFfdResampler<T, C>::resample(
   double    output_channel_value;
   double    bk3, bj3, bi3;
   int       ck, cj, ci;
-  bool      aberrantvalue = false;
   Point3dd  p_ref( output_location[0], output_location[1], output_location[2] );
 
   // transform point
@@ -1022,8 +1021,8 @@ NearestNeighborFfdResampler<T, C>::NearestNeighborFfdResampler(
     const FfdTransformation & transformation, const AffineTransformation3d & affine,
     T background ):
   NearestNeighborResampler<C>(),
-  _transformation(transformation),
   _affine(affine),
+  _transformation(transformation),
   _background(background)
 {
   init();
@@ -1112,8 +1111,8 @@ TrilinearFfdResampler<T, C>::TrilinearFfdResampler(
     const FfdTransformation & transformation, const AffineTransformation3d & affine,
     T background ):
   LinearResampler<C>(),
-  _transformation(transformation),
   _affine(affine),
+  _transformation(transformation),
   _background(background)
 {
   init();
