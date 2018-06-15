brainvisa/aims-free

commit a17d8e765f94c56ba5292ae2dc65a7b99b9b9fd8

Improve safety and const-correctness of the resampling APIs
git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/aims/aims-free/trunk@81950 613412ce-1361-4b7a-93bf-a3c75af1bae4

aimsalgo/src/aimsalgo/resampling/cubicresampler.h
@@ -57,4 +57,3 @@ AIMS_RESAMPLING_DECLARE_MULTICHANNELRESAMPLER( CubicResampler, AimsRGB, 3 )
 AIMS_RESAMPLING_DECLARE_MULTICHANNELRESAMPLER( CubicResampler, AimsRGBA, 3 )
 
 #endif
-

aimsalgo/src/aimsalgo/resampling/linearresampler.h
@@ -54,7 +54,7 @@ protected:
                    const Motion& transform3d,
                    const T& outBackground,
                    const Point3df& outLocation,
-                   T& outValue, int t );
+                   T& outValue, int t ) const;
   double getBSplineWeight( int i, double x ) const;
 };
 

aimsalgo/src/aimsalgo/resampling/masklinresampler.h
@@ -49,19 +49,19 @@ class MaskLinearResampler : public Resampler< T >
     MaskLinearResampler() : Resampler< T >() { };
     ~MaskLinearResampler() { }
 
-    void doit( const Motion& motion, AimsData<T>& thing );
+    void doit( const Motion& motion, AimsData<T>& thing ) const;
     AimsData<T> doit( const Motion& motion,int dimX, int dimY, int dimZ,
-                      const Point3df& resolution );
+                      const Point3df& resolution ) const;
 
   protected:
 
     void _sliceResamp( AimsData<T>& resamp, T* out,
 		       const Point3df& start, 
-                       int t, const AimsData<float>& Rinv );
+                       int t, const AimsData<float>& Rinv ) const;
 
     void 
     doResample( const AimsData< T > &, const Motion &, const T &, 
-                const Point3df &, T &, int ) {}
+                const Point3df &, T &, int ) const {}
 };
 
 
@@ -69,9 +69,9 @@ template <class T> inline
 AimsData<T> 
 MaskLinearResampler<T>::doit( const Motion& motion,
 			  int dimX, int dimY, int dimZ,
-			  const Point3df& resolution )
+			  const Point3df& resolution ) const
 {
-  ASSERT( this->_ref );
+  ASSERT( !this->_ref.isNull() );
 
   AimsData<T> thing( dimX, dimY, dimZ, this->_ref->dimT() );
   thing.setSizeXYZT( resolution[ 0 ], resolution[ 1 ], resolution[ 2 ],
@@ -85,9 +85,9 @@ MaskLinearResampler<T>::doit( const Motion& motion,
 
 template <class T> inline
 void MaskLinearResampler<T>::doit( const Motion& motion,
-				       AimsData<T>& thing )
+				       AimsData<T>& thing ) const
 {
-  ASSERT( this->_ref );
+  ASSERT( !this->_ref.isNull() );
   ASSERT( thing.dimT() == this->_ref->dimT() && thing.borderWidth() == 0 );
 
   Motion dirMotion = motion;
@@ -131,7 +131,7 @@ template <class T> inline
 void MaskLinearResampler<T>::_sliceResamp( AimsData<T>& resamp,
                                            T* out,
                                            const Point3df& start, int t,
-                                           const AimsData<float>& Rinv )
+                                           const AimsData<float>& Rinv ) const
 {
   typename AimsData<T>::const_iterator 
     pOrig = this->_ref->begin() + this->_ref->oFirstPoint() +

aimsalgo/src/aimsalgo/resampling/multichannelresampler.h
@@ -45,7 +45,7 @@ public: \
                          const Motion& transform3d, \
                          const T& outBackground, \
                          AimsData< T >& outVolume, \
-                         bool verbose = false ); \
+                         bool verbose = false ) const; \
 \
   int getOrder() const { return O; } \
 \
@@ -55,7 +55,7 @@ protected: \
   doResample( const AimsData< T > &, \
               const Motion &, \
               const T &, const Point3df &, \
-              T &, int ) {} \
+              T &, int ) const {} \
 \
 }; \
 
@@ -66,10 +66,9 @@ R< T >::resample( const AimsData< T >& inVolume, \
                   const Motion& transform3d, \
                   const T& outBackground, \
                   AimsData< T >& outVolume, \
-                  bool verbose ) \
+                  bool verbose ) const \
 { \
   ChannelSelector< AimsData<T>, AimsData<uint8_t> > selector; \
-  DataTypeInfo< T > info; \
 \
   int dimX = outVolume.dimX(); \
   int dimY = outVolume.dimY(); \

aimsalgo/src/aimsalgo/resampling/nearestneighborresampler.h
@@ -52,14 +52,13 @@ class NearestNeighborResampler : public Resampler< T >
     doResample( const AimsData< T > &inVolume, 
                 const Point3df &inLocation, 
                 const T &outBackground, 
-                T &outValue, int t );
+                T &outValue, int t ) const;
 
-    void 
+    virtual void
     doResample( const AimsData< T > &inVolume, 
                 const Motion &transform3d, 
                 const T &outBackground, const Point3df &outLocation, 
-                T &outValue, int t );
+                T &outValue, int t ) const;
 };
 
 #endif
-

aimsalgo/src/aimsalgo/resampling/quadraticresampler.h
@@ -57,4 +57,3 @@ AIMS_RESAMPLING_DECLARE_MULTICHANNELRESAMPLER( QuadraticResampler, AimsRGB, 2 )
 AIMS_RESAMPLING_DECLARE_MULTICHANNELRESAMPLER( QuadraticResampler, AimsRGBA, 2 )
 
 #endif
-

aimsalgo/src/aimsalgo/resampling/quinticresampler.h
@@ -57,4 +57,3 @@ AIMS_RESAMPLING_DECLARE_MULTICHANNELRESAMPLER( QuinticResampler, AimsRGB, 5 )
 AIMS_RESAMPLING_DECLARE_MULTICHANNELRESAMPLER( QuinticResampler, AimsRGBA, 5 )
 
 #endif
-

aimsalgo/src/aimsalgo/resampling/sixthorderresampler.h
@@ -57,4 +57,3 @@ AIMS_RESAMPLING_DECLARE_MULTICHANNELRESAMPLER( SixthOrderResampler, AimsRGB, 6 )
 AIMS_RESAMPLING_DECLARE_MULTICHANNELRESAMPLER( SixthOrderResampler, AimsRGBA, 6 )
 
 #endif
-

commit 6f653cc281efbbf002b513afae47075f2ae83f40

Fix the spline resamplers (broken in r81950)
git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/aims/aims-free/trunk@81986 613412ce-1361-4b7a-93bf-a3c75af1bae4

aimsalgo/src/aimsalgo/resampling/cubicresampler.h
@@ -46,11 +46,11 @@ public:
   CubicResampler();
   ~CubicResampler();
 
-  int getOrder() const;
+  int getOrder() const CARTO_OVERRIDE;
 
 protected:
 
-  double getBSplineWeight( int i, double x ) const;
+  double getBSplineWeight( int i, double x ) const CARTO_OVERRIDE;
 };
 
 AIMS_RESAMPLING_DECLARE_MULTICHANNELRESAMPLER( CubicResampler, AimsRGB, 3 )

aimsalgo/src/aimsalgo/resampling/linearresampler.h
@@ -54,8 +54,8 @@ protected:
                    const aims::Transformation3d& transform3d,
                    const T& outBackground,
                    const Point3df& outLocation,
-                   T& outValue, int t ) const;
-  double getBSplineWeight( int i, double x ) const;
+                   T& outValue, int t ) const CARTO_OVERRIDE;
+  double getBSplineWeight( int i, double x ) const CARTO_OVERRIDE;
 };
 
 AIMS_RESAMPLING_DECLARE_MULTICHANNELRESAMPLER( LinearResampler, AimsRGB, 1 )

aimsalgo/src/aimsalgo/resampling/masklinresampler.h
@@ -64,7 +64,7 @@ class MaskLinearResampler : public Resampler< T >
 
     void 
     doResample( const AimsData< T > &, const aims::Transformation3d &, const T &, 
-                const Point3df &, T &, int ) const
+                const Point3df &, T &, int ) const CARTO_OVERRIDE
     { throw std::runtime_error("not implemented"); }
 };
 

aimsalgo/src/aimsalgo/resampling/multichannelresampler.h
@@ -37,27 +37,30 @@
 #include <stdexcept>
 
 
+// FIXME: multi-channel resamplers inherit directly from Resampler<T>, they
+// should inherit from SplineResampler<T> instead.
 #define AIMS_RESAMPLING_DECLARE_MULTICHANNELRESAMPLER( R, T, O ) \
 template <> \
 class R< T > : public Resampler< T > \
 { \
 public: \
 \
-  virtual void resample( const AimsData< T >& inVolume, \
-                         const aims::AffineTransformation3d& transform3d, \
-                         const T& outBackground, \
-                         AimsData< T >& outVolume, \
-                         bool verbose = false ) const; \
+  void resample( const AimsData< T >& inVolume, \
+                 const aims::AffineTransformation3d& transform3d,       \
+                 const T& outBackground,                                \
+                 AimsData< T >& outVolume,                              \
+                 bool verbose = false ) const CARTO_OVERRIDE;           \
 \
   int getOrder() const { return O; } \
 \
 protected: \
 \
-  virtual void \
+  void \
   doResample( const AimsData< T > &, \
               const aims::Transformation3d &, \
               const T &, const Point3df &, \
-              T &, int ) const { throw std::runtime_error("not implemented");}; \
+              T &, int ) const CARTO_OVERRIDE \
+  { throw std::runtime_error("not implemented");}; \
 \
 }; \
 

aimsalgo/src/aimsalgo/resampling/nearestneighborresampler.h
@@ -54,11 +54,11 @@ class NearestNeighborResampler : public Resampler< T >
                 const T &outBackground, 
                 T &outValue, int t ) const;
 
-    virtual void
+    void
     doResample( const AimsData< T > &inVolume,
                 const aims::Transformation3d &transform3d,
                 const T &outBackground, const Point3df &outLocation,
-                T &outValue, int t ) const;
+                T &outValue, int t ) const CARTO_OVERRIDE;
 };
 
 #endif

aimsalgo/src/aimsalgo/resampling/quadraticresampler.h
@@ -46,11 +46,11 @@ public:
   QuadraticResampler();
   ~QuadraticResampler();
 
-  int getOrder() const;
+  int getOrder() const CARTO_OVERRIDE;
 
 protected:
 
-  double getBSplineWeight( int i, double x ) const;
+  double getBSplineWeight( int i, double x ) const CARTO_OVERRIDE;
 };
 
 AIMS_RESAMPLING_DECLARE_MULTICHANNELRESAMPLER( QuadraticResampler, AimsRGB, 2 )

aimsalgo/src/aimsalgo/resampling/quarticresampler.h
@@ -46,11 +46,11 @@ public:
   QuarticResampler();
   ~QuarticResampler();
 
-  int getOrder() const;
+  int getOrder() const CARTO_OVERRIDE;
 
 protected:
 
-  double getBSplineWeight( int i, double x ) const;
+  double getBSplineWeight( int i, double x ) const CARTO_OVERRIDE;
 };
 
 AIMS_RESAMPLING_DECLARE_MULTICHANNELRESAMPLER( QuarticResampler, AimsRGB, 4 )

aimsalgo/src/aimsalgo/resampling/quinticresampler.h
@@ -46,11 +46,11 @@ public:
   QuinticResampler();
   ~QuinticResampler();
 
-  int getOrder() const;
+  int getOrder() const CARTO_OVERRIDE;
 
 protected:
 
-  double getBSplineWeight( int i, double x ) const;
+  double getBSplineWeight( int i, double x ) const CARTO_OVERRIDE;
 };
 
 AIMS_RESAMPLING_DECLARE_MULTICHANNELRESAMPLER( QuinticResampler, AimsRGB, 5 )

aimsalgo/src/aimsalgo/resampling/seventhorderresampler.h
@@ -46,11 +46,11 @@ public:
   SeventhOrderResampler();
   ~SeventhOrderResampler();
 
-  int getOrder() const;
+  int getOrder() const CARTO_OVERRIDE;
 
 protected:
 
-  double getBSplineWeight( int i, double x ) const;
+  double getBSplineWeight( int i, double x ) const CARTO_OVERRIDE;
 };
 
 AIMS_RESAMPLING_DECLARE_MULTICHANNELRESAMPLER( SeventhOrderResampler, AimsRGB, 7 )

aimsalgo/src/aimsalgo/resampling/sixthorderresampler.h
@@ -46,11 +46,11 @@ public:
   SixthOrderResampler();
   ~SixthOrderResampler();
 
-  int getOrder() const;
+  int getOrder() const CARTO_OVERRIDE;
 
 protected:
 
-  double getBSplineWeight( int i, double x ) const;
+  double getBSplineWeight( int i, double x ) const CARTO_OVERRIDE;
 };
 
 AIMS_RESAMPLING_DECLARE_MULTICHANNELRESAMPLER( SixthOrderResampler, AimsRGB, 6 )

aimsalgo/src/aimsalgo/resampling/splineresampler.h
@@ -122,12 +122,10 @@ protected:
                    const T &outBackground,
                    const Point3df &outLocation,
                    T &outValue,
-                   int t ) const;
+                   int t ) const CARTO_OVERRIDE;
 
-  // FIXME: this method should be const in order to override the virtual method
-  // of the base class
   void updateParameters( const AimsData< T >& inVolume, int t,
-                         bool verbose );
+                         bool verbose ) const CARTO_OVERRIDE;
   void iirConvolveMirror( std::vector< double >& data ) const;
 
   // This method returns a mirror index when needed
@@ -148,9 +146,11 @@ protected:
 
   std::vector<double>  _poles;
   double               _gain;
-  AimsData<double>     _splineCoefficients;
-  const AimsData<T> *  _lastvolume;
-  int                  _lasttime;
+
+  // These three mutable members handle the cache of spline coefficients
+  mutable AimsData<double>     _splineCoefficients;
+  mutable const AimsData<T> *  _lastvolume;
+  mutable int                  _lasttime;
 };
 
 namespace aims {

aimsalgo/src/aimsalgo/resampling/splineresampler_d.h
@@ -277,7 +277,7 @@ SplineResampler< T >::doResample( const AimsData< T >& inVolume,
 template < class T >
 void
 SplineResampler< T >::updateParameters( const AimsData< T >& inVolume,
-                                        int t, bool verbose )
+                                        int t, bool verbose ) const
 {
 
   if ( ( &inVolume != _lastvolume || t != _lasttime )