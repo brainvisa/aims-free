4778f55e31614caf1efe7edf53c9c137f4adb08f
ylep <yann.leprince@cea.fr>
2018-06-20 06:21:24 +0000

    Add a TransformationChain3d class for chaining transformations
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/aims/aims-free/trunk@82012 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ aimsdata/src/aimsdata/transformation/transformation_chain.cc
@@ -0,0 +1,94 @@
+/* This software and supporting documentation are distributed by
+ *     Institut Federatif de Recherche 49
+ *     CEA/NeuroSpin, Batiment 145,
+ *     91191 Gif-sur-Yvette cedex
+ *     France
+ *
+ * This software is governed by the CeCILL-B license under
+ * French law and abiding by the rules of distribution of free software.
+ * You can  use, modify and/or redistribute the software under the
+ * terms of the CeCILL-B license as circulated by CEA, CNRS
+ * and INRIA at the following URL "http://www.cecill.info".
+ *
+ * As a counterpart to the access to the source code and  rights to copy,
+ * modify and redistribute granted by the license, users are provided only
+ * with a limited warranty  and the software's author,  the holder of the
+ * economic rights,  and the successive licensors  have only  limited
+ * liability.
+ *
+ * In this respect, the user's attention is drawn to the risks associated
+ * with loading,  using,  modifying and/or developing or reproducing the
+ * software by the user in light of its specific status of free software,
+ * that may mean  that it is complicated to manipulate,  and  that  also
+ * therefore means  that it is reserved for developers  and  experienced
+ * professionals having in-depth computer knowledge. Users are therefore
+ * encouraged to load and test the software's suitability as regards their
+ * requirements in conditions enabling the security of their systems and/or
+ * data to be ensured and,  more generally, to use and operate it in the
+ * same conditions as regards security.
+ *
+ * The fact that you are presently reading this means that you have had
+ * knowledge of the CeCILL-B license and that you accept its terms.
+ */
+
+#include <aims/transformation/transformation_chain.h>
+
+void aims::TransformationChain3d::
+push_last(const carto::const_ref<Transformation3d>& transformation)
+{
+  _transformations.push_back(transformation);
+}
+
+void aims::TransformationChain3d::pop_last()
+{
+  _transformations.pop_back();
+}
+
+void aims::TransformationChain3d::
+push_first(const carto::const_ref<Transformation3d>& transformation)
+{
+  _transformations.push_front(transformation);
+}
+
+void aims::TransformationChain3d::pop_first()
+{
+  _transformations.pop_front();
+}
+
+Point3dd aims::TransformationChain3d::
+transformPoint3dd( const Point3dd& point ) const
+{
+  Point3dd transformed_point = point;
+  for(ListType::const_iterator it = _transformations.begin() ;
+      it != _transformations.end() ;
+      ++it)
+  {
+    transformed_point = (*it)->transform(transformed_point);
+  }
+  return transformed_point;
+}
+
+Point3df aims::TransformationChain3d::
+transformPoint3df( const Point3df& point ) const
+{
+  Point3df transformed_point = point;
+  for(ListType::const_iterator it = _transformations.begin() ;
+      it != _transformations.end() ;
+      ++it)
+  {
+    transformed_point = (*it)->transform(transformed_point);
+  }
+  return transformed_point;
+}
+
+Point3dd aims::TransformationChain3d::
+transformDouble( double x, double y, double z ) const
+{
+  return transformPoint3dd(Point3dd(x, y, z));
+}
+
+Point3df aims::TransformationChain3d::
+transformFloat( float x, float y, float z ) const
+{
+  return transformPoint3df(Point3df(x, y, z));
+}
