acfbacfa1e38305cae4f00b5ce40fbc44b75328e
denisri <denis.riviere.gm@gmail.com>
2018-08-28 12:11:58 +0000

    bind AimsData volumes for bool
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/aims/aims-free/trunk@82906 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- pyaims/src/sip/typessub.py
+++ pyaims/src/sip/typessub.py
@@ -946,6 +946,30 @@ typessub = {'bool':
                'testPyType': 'pyaimsGenericObject_Check',
             },
 
+            'AimsData<bool>':
+           {'typecode': 'AimsData_BOOL',
+               'pyFromC': '',
+               'CFromPy': '',
+               'castFromSip': '',
+               'deref': '*',
+               'pyderef': '*',
+               'address': '&',
+               'pyaddress': '&',
+               'defScalar': '',
+               'defNumpyBindings': '',
+               'new': 'new AimsData<bool>',
+               'NumType': 'PyArray_OBJECT',
+               'PyType': 'AimsData_BOOL',
+               'sipClass': 'AimsData_BOOL',
+               'typeinclude': '#include <aims/data/data.h>',
+               'sipinclude': '#ifndef PYAIMS_AIMSDATA_BOOL_CHECK_DEFINED\n'
+               '#define PYAIMS_AIMSDATA_BOOL_CHECK_DEFINED\n'
+               'inline int pyaimsAimsData_BOOL_Check( PyObject* o )\n'
+               '{ return sipCanConvertToType( o, sipFindType( "AimsData_BOOL" ), SIP_NOT_NONE | SIP_NO_CONVERTORS ); }\n'
+               '#endif',
+               'module': 'aims',
+               'testPyType': 'pyaimsAimsData_BOOL_Check',
+            },
             'AimsData<int8_t>':
            {'typecode': 'AimsData_S8',
                'pyFromC': '',
