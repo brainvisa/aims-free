brainvisa/aims-free

commit 406926867d7a7be977960ceb6da10d1e87243173

add virtual methods for inverting instances of Transformation3d
git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/aims/aims-free/trunk@83294 613412ce-1361-4b7a-93bf-a3c75af1bae4

aimsalgo/src/AimsRegisterFFDApply/applydef.cc
@@ -585,11 +585,21 @@ bool doBucket( Process & process, const string & fileref, Finder & )
   //      Do resampling
   //
   //==========================================================================
-  cout << "Resampling ";
 
-  rc_ptr<BucketMap<Void> > out
-    = transformBucketDirect( in, *transform,
-                             Point3df( ffdproc.sx, ffdproc.sy, ffdproc.sz ) );
+  rc_ptr<BucketMap<Void> > out;
+
+  if(transform->invertible()) {
+    cout << "Resampling the Bucket with the combined pushforward and pullback "
+      "methods... ";
+    rc_ptr<Transformation3d> inverse_transform = transform->getInverse();
+    out = resampleBucket( in, *transform, *inverse_transform,
+                          Point3df( ffdproc.sx, ffdproc.sy, ffdproc.sz ) );
+  } else {
+    cout << "Transforming the Bucket with the low-quality pushforward "
+      "method... ";
+    out = transformBucketDirect( in, *transform,
+                                 Point3df( ffdproc.sx, ffdproc.sy, ffdproc.sz ) );
+  }
 
   cout << endl;
 
@@ -707,9 +717,13 @@ bool doGraph( Process & process, const string & fileref, Finder & f )
   //      Do resampling
   //
   //==========================================================================
-  cout << "Resampling ";
+  cout << "Resampling the Graph... ";
+  rc_ptr<Transformation3d> inverse_transform;
 
-  transformGraph( *in, *transform, NULL,
+  if(transform->invertible()) {
+    inverse_transform = transform->getInverse();
+  }
+  transformGraph( *in, *transform, inverse_transform.get(),
                   Point3df( ffdproc.sx, ffdproc.sy, ffdproc.sz ) );
 
   cout << endl;

aimsdata/src/aimsdata/transformation/transformation_chain.cc
@@ -68,6 +68,36 @@ bool aims::TransformationChain3d::isIdentity() const
   return true;
 }
 
+bool aims::TransformationChain3d::invertible() const
+{
+  for(ListType::const_iterator it = _transformations.begin() ;
+      it != _transformations.end() ;
+      ++it)
+  {
+    if(!(*it)->invertible()) {
+      return false;
+    }
+  }
+  return true;
+}
+
+std::unique_ptr<aims::Transformation3d>
+aims::TransformationChain3d::getInverse() const
+{
+  std::unique_ptr<aims::Transformation3d> ret(new aims::TransformationChain3d);
+  aims::TransformationChain3d & new_chain
+    = dynamic_cast<aims::TransformationChain3d&>(*ret.get());
+
+  for(ListType::const_reverse_iterator rit = _transformations.rbegin() ;
+      rit != _transformations.rend() ;
+      ++rit)
+  {
+    new_chain.push_back((*rit)->getInverse());
+  }
+
+  return ret;
+}
+
 Point3dd aims::TransformationChain3d::
 transformPoint3dd( const Point3dd& point ) const
 {

aimsdata/src/aimsdata/transformation/affinetransformation3d.cc
@@ -513,6 +513,21 @@ AffineTransformation3d AffineTransformation3d::inverse() const
   return AffineTransformation3d;
 }
 
+bool AffineTransformation3d::invertible() const
+{
+  // inverse() will throw carto::assert_error if matrix inversion fails
+  try {
+    inverse();
+  } catch(const assert_error&) {
+    return false;
+  }
+  return true;
+}
+
+unique_ptr<Transformation3d> AffineTransformation3d::getInverse() const
+{
+  return unique_ptr<Transformation3d>(new AffineTransformation3d(inverse()));
+}
 
 
 //-----------------------------------------------------------------------------

aimsdata/src/aimsdata/transformation/affinetransformation3d.h
@@ -154,6 +154,8 @@ public:
 
   // AffineTransformation3d algebraic operation
   AffineTransformation3d inverse() const;
+  bool invertible() const CARTO_OVERRIDE;
+  std::unique_ptr<Transformation3d> getInverse() const CARTO_OVERRIDE;
   virtual void scale( const Point3df& sizeFrom, const Point3df& sizeTo );
   /// true if the transformation is direct, false if it changes orientation
   bool isDirect() const;

aimsdata/src/aimsdata/transformation/transformation.h
@@ -34,6 +34,8 @@
 #ifndef AIMS_TRANSFORMATION_TRANSFORMATION_H
 #define AIMS_TRANSFORMATION_TRANSFORMATION_H
 
+#include <stdexcept>
+
 #include <aims/vector/vector.h>
 #include <cartobase/smart/rcptr.h>
 
@@ -80,6 +82,24 @@ namespace aims
     Point3df transform( const Point3df & dir ) const;
     Point3df transform( float x, float y, float z ) const;
     Point3d transform( const Point3d & p ) const;
+
+    /** Test if the transformation can be inverted
+
+        getInverse() can be called if this method returns true, in order to
+        obtain the inverse transformation.
+     */
+    virtual bool invertible() const {
+      return false;
+    };
+    /** Obtain the inverse transformation
+
+        This method should only be called if invertible() returns true.
+        Otherwise, it will throw an exception if the transformation is not
+        actually invertible.
+     */
+    virtual std::unique_ptr<Transformation3d> getInverse() const {
+      throw std::logic_error("not implemented");
+    };
 
   protected:
     Transformation3d() {}

aimsdata/src/aimsdata/transformation/transformation_chain.h
@@ -76,6 +76,8 @@ public:
   void pop_front();
 
   bool isIdentity() const CARTO_OVERRIDE;
+  bool invertible() const CARTO_OVERRIDE;
+  std::unique_ptr<Transformation3d> getInverse() const CARTO_OVERRIDE;
 
 protected:
   ListType _transformations;

commit 5435f71f1a8d91cb200c42c3fc24057131923090

fixes: build issue about unique_ptr conversion in C++11
This fixes both the build problem of C++98 mode introduced in r83294, and the build problem of C++11 mode introduced in r83430.


git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/aims/aims-free/trunk@83443 613412ce-1361-4b7a-93bf-a3c75af1bae4

aimsalgo/src/AimsApplyTransform/AimsApplyTransform.cc
@@ -258,13 +258,11 @@ load_transformations(const ApplyTransformProc& proc)
   }
 
   if(!ret.first && ret.second->invertible()) {
-    std::unique_ptr<aims::Transformation3d> inv = ret.second->getInverse();
-    ret.first = inv;
+    ret.first = rc_ptr<Transformation3d>(ret.second->getInverse());
   }
 
   if(!ret.second && ret.first->invertible()) {
-    std::unique_ptr<aims::Transformation3d> inv = ret.first->getInverse();
-    ret.second = inv;
+    ret.second = rc_ptr<Transformation3d>(ret.first->getInverse());
   }
 
   return ret;

aimsalgo/src/AimsRegisterFFDApply/applydef.cc
@@ -591,7 +591,7 @@ bool doBucket( Process & process, const string & fileref, Finder & )
   if(transform->invertible()) {
     cout << "Resampling the Bucket with the combined pushforward and pullback "
       "methods... ";
-    std::unique_ptr<Transformation3d> inverse_transform = transform->getInverse();
+    rc_ptr<Transformation3d> inverse_transform(transform->getInverse());
     out = resampleBucket( in, *transform, *inverse_transform,
                           Point3df( ffdproc.sx, ffdproc.sy, ffdproc.sz ) );
   } else {
@@ -718,10 +718,10 @@ bool doGraph( Process & process, const string & fileref, Finder & f )
   //
   //==========================================================================
   cout << "Resampling the Graph... ";
-  std::unique_ptr<aims::Transformation3d> inverse_transform;
+  rc_ptr<Transformation3d> inverse_transform;
 
   if(transform->invertible()) {
-    inverse_transform = transform->getInverse();
+    inverse_transform = rc_ptr<Transformation3d>(transform->getInverse());
   }
   transformGraph( *in, *transform, inverse_transform.get(),
                   Point3df( ffdproc.sx, ffdproc.sy, ffdproc.sz ) );

aimsdata/src/aimsdata/transformation/transformation_chain.cc
@@ -92,8 +92,9 @@ aims::TransformationChain3d::getInverse() const
       rit != _transformations.rend() ;
       ++rit)
   {
-    std::unique_ptr<aims::Transformation3d> inv((*rit)->getInverse());
-    new_chain.push_back(inv);
+    new_chain.push_back(carto::rc_ptr<Transformation3d>(
+      (*rit)->getInverse())
+    );
   }
 
   return ret;