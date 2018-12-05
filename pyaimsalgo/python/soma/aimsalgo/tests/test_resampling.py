brainvisa/aims-free

commit 4b34de2139f86354c2cd6db4348a06c97e108156

changed the API of resamplers to use carto::Volume instead of AimsData
git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/aims/aims-free/trunk@82911 613412ce-1361-4b7a-93bf-a3c75af1bae4

aimsalgo/src/aimsalgo/resampling/resampler_d.h
@@ -59,33 +59,35 @@ namespace aims
 
 template <typename T>
 Resampler<T>::Resampler()
-  : _ref(0, 0, 0, 0), _defval( T() )
+  : _ref( 0 ), _defval( T() )
 {
 }
 
 
 template <typename T>
 void Resampler<T>::
-resample_inv_to_vox( const AimsData< T >& inVolume,
+resample_inv_to_vox( const carto::Volume< T >& inVolume,
                      const aims::Transformation3d& inverse_transform_to_vox,
                      const T& outBackground,
-                     AimsData< T >& outVolume,
+                     carto::Volume< T > & outVolume,
                      bool verbose ) const
 {
 
   Point3df outResolution;
-  outResolution[0] = outVolume.sizeX();
-  outResolution[1] = outVolume.sizeY();
-  outResolution[2] = outVolume.sizeZ();
+  std::vector<float> vs = outVolume.getVoxelSize();
+  outResolution[0] = vs[0];
+  outResolution[1] = vs[1];
+  outResolution[2] = vs[2];
 
-  int outSizeX = outVolume.dimX();
-  int outSizeY = outVolume.dimY();
-  int outSizeZ = outVolume.dimZ();
-  int outSizeT = outVolume.dimT();
-  if( outSizeT > inVolume.dimT() )
-    outSizeT = inVolume.dimT();
+  std::vector<int> sz = outVolume.getSize();
+  int outSizeX = sz[0];
+  int outSizeY = sz[1];
+  int outSizeZ = sz[2];
+  int outSizeT = sz[3];
+  if( outSizeT > inVolume.getVoxelSize()[3] )
+    outSizeT = inVolume.getVoxelSize()[3];
 
-  typename AimsData< T >::iterator o;
+  T* o;
 
   aims::Progression progress(0, static_cast<size_t>(outSizeX)
                              * outSizeY * outSizeZ * outSizeT);
@@ -101,7 +103,7 @@ resample_inv_to_vox( const AimsData< T >& inVolume,
 
           for ( y = 0; y < outSizeY; y++ )
             {
-              o = &outVolume( 0, y, z, t );
+              o = &outVolume.at( 0, y, z, t );
 
               for ( x = 0; x < outSizeX; x++ )
                 {
@@ -132,16 +134,14 @@ resample_inv_to_vox( const AimsData< T >& inVolume,
 
 template <typename T>
 void Resampler<T>::
-resample_inv( const AimsData< T >& input_data,
+resample_inv( const carto::Volume< T >& input_data,
               const aims::Transformation3d& inverse_transform_to_mm,
               const T& background,
-              AimsData< T >& output_volume,
+              carto::Volume< T > & output_volume,
               bool verbose ) const
 {
 
-  Point3df in_voxel_size(input_data.sizeX(),
-                         input_data.sizeY(),
-                         input_data.sizeZ());
+  Point3df in_voxel_size( input_data.getVoxelSize() );
 
   aims::AffineTransformation3d mm_to_voxel_transform;
   mm_to_voxel_transform.scale( Point3df( 1, 1, 1 ), in_voxel_size );
@@ -156,7 +156,7 @@ resample_inv( const AimsData< T >& input_data,
 
 template <typename T>
 void Resampler<T>::
-resample_inv( const AimsData< T >& input_data,
+resample_inv( const carto::Volume< T >& input_data,
               const aims::Transformation3d& inverse_transform_to_mm,
               const T& background,
               const Point3df& output_location,
@@ -164,9 +164,7 @@ resample_inv( const AimsData< T >& input_data,
               int timestep ) const
 {
 
-  Point3df in_voxel_size(input_data.sizeX(),
-                         input_data.sizeY(),
-                         input_data.sizeZ());
+  Point3df in_voxel_size( input_data.getVoxelSize() );
 
   aims::AffineTransformation3d mm_to_voxel_transform;
   mm_to_voxel_transform.scale( Point3df( 1, 1, 1 ), in_voxel_size );
@@ -182,17 +180,14 @@ resample_inv( const AimsData< T >& input_data,
 
 template < typename T >
 void
-Resampler< T >::resample( const AimsData< T >& inVolume,
+Resampler< T >::resample( const carto::Volume< T >& inVolume,
                           const aims::AffineTransformation3d& transform3d,
                           const T& outBackground,
-                          AimsData< T >& outVolume,
+                          carto::Volume< T > & outVolume,
                           bool verbose ) const
 {
 
-  Point3df inResolution;
-  inResolution[0] = inVolume.sizeX();
-  inResolution[1] = inVolume.sizeY();
-  inResolution[2] = inVolume.sizeZ();
+  Point3df inResolution( inVolume.getVoxelSize() );
 
   aims::AffineTransformation3d normTransform3d;
 
@@ -208,17 +203,14 @@ Resampler< T >::resample( const AimsData< T >& inVolume,
 
 template < typename T >
 void
-Resampler< T >::resample( const AimsData< T >& inVolume,
+Resampler< T >::resample( const carto::Volume< T >& inVolume,
                           const aims::AffineTransformation3d& transform3d,
                           const T& outBackground,
                           const Point3df& outLocation,
                           T& outValue, int t ) const
 {
 
-  Point3df inResolution;
-  inResolution[0] = inVolume.sizeX();
-  inResolution[1] = inVolume.sizeY();
-  inResolution[2] = inVolume.sizeZ();
+  Point3df inResolution( inVolume.getVoxelSize() );
 
   aims::AffineTransformation3d normTransform3d;
 
@@ -236,85 +228,83 @@ Resampler< T >::resample( const AimsData< T >& inVolume,
 
 template <typename T>
 void Resampler<T>::doit( const aims::AffineTransformation3d& motion,
-                         AimsData<T>& thing ) const
+                         carto::Volume<T> & thing ) const
 {
-  if( _ref.empty() )
+  if( _ref.isNull() )
     throw std::runtime_error( "Resampler used without a ref volume to resample"
     );
-  resample( _ref, motion, _defval, thing, carto::verbose );
+  resample( *_ref, motion, _defval, thing, carto::verbose );
 }
 
 
 template <typename T>
-AimsData<T> Resampler<T>::doit( const aims::AffineTransformation3d& motion,
-                                int dimX, int dimY, int dimZ,
-                                const Point3df& resolution ) const
+carto::VolumeRef<T> Resampler<T>::doit(
+  const aims::AffineTransformation3d& motion,
+  int dimX, int dimY, int dimZ,
+  const Point3df& resolution ) const
 {
-  if( _ref.empty() )
+  if( _ref.isNull() )
     throw std::runtime_error( "Resampler used without a ref volume to resample"
     );
-  AimsData<T>	thing( dimX, dimY, dimZ, _ref.dimT() );
-  thing.setSizeXYZT( resolution[0], resolution[1], resolution[2],
-                     _ref.sizeT() );
+  carto::VolumeRef<T>	thing( dimX, dimY, dimZ, _ref->getSizeT() );
+  std::vector<float> vs( 4, 1. );
+  vs[0] = resolution[0];
+  vs[1] = resolution[1];
+  vs[2] = resolution[2];
+  vs[3] = _ref->getVoxelSize()[3];
+  thing->header().setProperty( "voxel_size", vs );
 
-  resample( _ref, motion, _defval, thing, carto::verbose );
-  thing.setHeader( _ref.header()->cloneHeader() );
-  thing.setSizeXYZT( resolution[0], resolution[1], resolution[2],
-                     _ref.sizeT() );
+  resample( *_ref, motion, _defval, *thing, carto::verbose );
+  thing->copyHeaderFrom( _ref->header() );
+  thing->header().setProperty( "voxel_size", vs );
   if( !motion.isIdentity() )
   {
-    aims::PythonHeader
-        *ph = dynamic_cast<aims::PythonHeader *>( thing.header() );
+    carto::PropertySet & ph = thing->header();
     try
     {
       // remove any referential ID since we are no longer in the same ref
-      ph->removeProperty( "referential" );
+      ph.removeProperty( "referential" );
     }
     catch( ... )
     {
     }
     try
     {
-      if( ph )
+      carto::Object trs = ph.getProperty( "transformations" );
+      carto::Object tit = trs->objectIterator();
+      aims::AffineTransformation3d motioninv = motion.inverse();
+      std::vector<std::vector<float> > trout;
+      trout.reserve( trs->size() );
+      for( ; tit->isValid(); tit->next() )
       {
-        carto::Object trs = ph->getProperty( "transformations" );
-        carto::Object tit = trs->objectIterator();
-        aims::AffineTransformation3d motioninv = motion.inverse();
-        std::vector<std::vector<float> > trout;
-        trout.reserve( trs->size() );
-        for( ; tit->isValid(); tit->next() )
-        {
-          aims::AffineTransformation3d m( tit->currentValue() );
-          m *= motioninv;
-          trout.push_back( m.toVector() );
-        }
-        ph->setProperty( "transformations", trout );
+        aims::AffineTransformation3d m( tit->currentValue() );
+        m *= motioninv;
+        trout.push_back( m.toVector() );
       }
+      ph.setProperty( "transformations", trout );
     }
     catch( ... )
     {
       // setup a new transformations list
       std::vector<std::vector<float> > trout;
       std::vector<std::string> refsout;
-      const aims::PythonHeader
-          *iph = dynamic_cast<const aims::PythonHeader *>( _ref.header() );
-      if( iph )
-        try
-        {
-          carto::Object iref = iph->getProperty( "referential" );
-          std::string refid = iref->getString();
-          refsout.push_back( refid );
-        }
-        catch( ... )
-        {
-        }
+      const carto::PropertySet & iph = _ref->header();
+      try
+      {
+        carto::Object iref = iph.getProperty( "referential" );
+        std::string refid = iref->getString();
+        refsout.push_back( refid );
+      }
+      catch( ... )
+      {
+      }
       if( refsout.empty() )
         refsout.push_back( "Coordinates aligned to another file or to "
             "anatomical truth" );
 
       trout.push_back( motion.inverse().toVector() );
-      ph->setProperty( "transformations", trout );
-      ph->setProperty( "referentials", refsout );
+      ph.setProperty( "transformations", trout );
+      ph.setProperty( "referentials", refsout );
     }
   }
   return thing;
@@ -322,7 +312,7 @@ AimsData<T> Resampler<T>::doit( const aims::AffineTransformation3d& motion,
 
 
 template <typename T>
-void Resampler<T>::setRef(const AimsData<T>& ref)
+void Resampler<T>::setRef(const carto::rc_ptr<carto::Volume<T> >& ref)
 {
   _ref = ref;
 }

aimsalgo/src/AimsMIRegister/objfunc.h
@@ -206,7 +206,7 @@ void ObjFunc::doPdf( const AimsVector<float,6>& param ) const
       ASSERT( _interpolator );  // il doit exister necesaire un interpolateur
       _interpolator->setRef( *_test );
       
-      _interpolator->doit( motion, *_reg );
+      _interpolator->doit( motion, *_reg->volume() );
       AimsJointMaskPdf( *_ref, *_reg, *_p12, *_p1, *_p2 );
     }
 }

aimsalgo/src/AimsRegisterFFDApply/applydef.cc
@@ -229,9 +229,14 @@ bool doVolume( Process & process, const string & fileref, Finder & )
   //--------------------------------------------------------------------------
   // Output resampled volume
   //--------------------------------------------------------------------------
-  AimsData<T> out( ffdproc.dx, ffdproc.dy, ffdproc.dz, in.dimT() );
-  out.setSizeXYZT( ffdproc.sx, ffdproc.sy, ffdproc.sz, in.sizeT() );
-  out = bv;
+  VolumeRef<T> out( ffdproc.dx, ffdproc.dy, ffdproc.dz, in.dimT() );
+  vector<float> vs( 4 );
+  vs[0] = ffdproc.sx;
+  vs[1] = ffdproc.sy;
+  vs[2] = ffdproc.sz;
+  vs[3] = in.sizeT();
+  out->header().setProperty( "voxel_size", vs );
+  out->fill( bv );
 
   //==========================================================================
   //
@@ -258,7 +263,7 @@ bool doVolume( Process & process, const string & fileref, Finder & )
   cout << "Initialization done." << endl;
 
   cout << "Resampling... ";
-  rsp->resample_inv( in, *transform, bv, out, true );
+  rsp->resample_inv( *in.volume(), *transform, bv, *out, true );
   cout << endl;
 
   if(!ffdproc.compout.empty()
@@ -269,7 +274,7 @@ bool doVolume( Process & process, const string & fileref, Finder & )
   //--------------------------------------------------------------------------
   // Output resampled volume
   //--------------------------------------------------------------------------
-  Writer<AimsData<T> > w3(ffdproc.output);
+  Writer<VolumeRef<T> > w3(ffdproc.output);
   success = w3.write(out) && success;
 
   return success;

aimsalgo/src/AimsRegisterFFDEstimate/dataModel.cc
@@ -42,31 +42,33 @@ DataModel::~DataModel()
 }
 
 DataModel::DataModel( BucketMap< Void >&                s,
-                      AimsData< short >&                r,
-                      AimsData< short >&                t,
+                      VolumeRef< short >                r,
+                      VolumeRef< short >                t,
                       Motion&                           m,
                       SplineFfd&                        d,
                       ParzenProbDensFunction&           pdf,
                       bool                              prepro ):
+    _current( ((t->getSizeX() != 1) + (t->getSizeY() != 1) + (t->getSizeZ() != 1))
+              * d.getSizeX() * d.getSizeY() * d.getSizeZ(), 0.),
     _deformation( d ),
     _rigid( m ),
     _pdf( pdf ),
+    _spline(3, 1),
+    _partDer( ((t->getSizeX() != 1) + (t->getSizeY() != 1) + (t->getSizeZ() != 1))
+              * d.getSizeX() * d.getSizeY() * d.getSizeZ(), 0.),
     _reference( r ),
     _test( t ),
-    _dimParam( (t.dimX() != 1) + (t.dimY() != 1) + (t.dimZ() != 1) ),
-    _current( ((t.dimX() != 1) + (t.dimY() != 1) + (t.dimZ() != 1))
-              * d.dimX() * d.dimY() * d.dimZ(), 0.),
-    _partDer( ((t.dimX() != 1) + (t.dimY() != 1) + (t.dimZ() != 1))
-              * d.dimX() * d.dimY() * d.dimZ(), 0.),
-    _contrib( r.dimX(), r.dimY(), r.dimZ() ),
-    _is2d( t.dimX() == 1, t.dimY() == 1, t.dimZ() == 1 ),
-    _spline(3, 1)
+    _is2d( t->getSizeX() == 1, t->getSizeY() == 1, t->getSizeZ() == 1 ),
+    _dimParam( (t->getSizeX() != 1) + (t->getSizeY() != 1) + (t->getSizeZ() != 1) ),
+    _contrib( r->getSizeX(), r->getSizeY(), r->getSizeZ() )
 {
   // debug
-  _contrib.setSizeXYZT( r.sizeX(), r.sizeY(), r.sizeZ(), r.sizeT() );
+  _contrib->header().setProperty( "voxel_size", r->getVoxelSize() );
 
   // Compute histo parameter + replace min max values
-  _pdf.updateBinSizeAndMin( r, t, prepro );
+  AimsData<short> ar( r ); // FIXME remove this after updateBinSizeAndMin changes
+  AimsData<short> at( t );
+  _pdf.updateBinSizeAndMin( ar, at, prepro );
 
   // Construction des coefficients splines pour l'image test
   CubicResampler<short> interpolator;
@@ -76,16 +78,20 @@ DataModel::DataModel( BucketMap< Void >&                s,
   // Initialize evaluation points
   //
 
-  this->_selection.setSizeXYZT( r.sizeX(),
-                                r.sizeY(),
-                                r.sizeZ(),
-                                r.sizeT() );
+  vector<float> vs = r->getVoxelSize();
+  this->_selection.setSizeXYZT( vs[0],
+                                vs[1],
+                                vs[2],
+                                vs[3] );
+
+  vector<int> sdim = _splineTest->getSize();
 
   if (s.size() > 0)
   {
     // Use input bucket points
     BucketMap<Void>::Bucket bucket = s.begin()->second;
     BucketMap<Void>::Bucket::iterator ib, eb = bucket.end();
+    vector<float> tvs = _test->getVoxelSize();
 
     for( ib = bucket.begin(); ib != eb; ++ib )
     {
@@ -93,14 +99,14 @@ DataModel::DataModel( BucketMap< Void >&                s,
                          float(ib->first[1]) * s.sizeY(),
                          float(ib->first[2]) * s.sizeZ() );
       Point3df p_test_mm = m.transform( p_ref_mm );
-      Point3df p_test_vox( p_test_mm[0] / _test.sizeX(),
-                           p_test_mm[1] / _test.sizeY(),
-                           p_test_mm[2] / _test.sizeZ() );
+      Point3df p_test_vox( p_test_mm[0] / tvs[0],
+                           p_test_mm[1] / tvs[1],
+                           p_test_mm[2] / tvs[2] );
 
       // On saute les bords car interpolation imparfaite
-      if( 0 <= p_test_vox[0] && p_test_vox[0] < _splineTest.dimX() &&
-          0 <= p_test_vox[1] && p_test_vox[1] < _splineTest.dimY() &&
-          0 <= p_test_vox[2] && p_test_vox[2] < _splineTest.dimZ() )
+      if( 0 <= p_test_vox[0] && p_test_vox[0] < sdim[0] &&
+          0 <= p_test_vox[1] && p_test_vox[1] < sdim[1] &&
+          0 <= p_test_vox[2] && p_test_vox[2] < sdim[2] )
       {
         this->_selection.insert(ib->first, p_test_mm);
       }
@@ -111,19 +117,21 @@ DataModel::DataModel( BucketMap< Void >&                s,
     // No available points in bucket map
     // Default use of the reference image
     int i, j, k;
-    ForEach3d(r, i, j, k) {
+    ForEach3d(ar, i, j, k) {
       Point3d a(i, j, k);
-      Point3df p_ref_mm( float(a[0]) * _reference.sizeX(),
-                         float(a[1]) * _reference.sizeY(),
-                         float(a[2]) * _reference.sizeZ() );
+      vector<float> vs = _reference->getVoxelSize();
+      Point3df p_ref_mm( float(a[0]) * vs[0],
+                         float(a[1]) * vs[1],
+                         float(a[2]) * vs[2] );
       Point3df p_test_mm = m.transform( p_ref_mm );
-      Point3df p_test_vox( p_test_mm[0] / _test.sizeX(),
-                           p_test_mm[1] / _test.sizeY(),
-                           p_test_mm[2] / _test.sizeZ() );
+      vector<float> tvs = _test->getVoxelSize();
+      Point3df p_test_vox( p_test_mm[0] / tvs[0],
+                           p_test_mm[1] / tvs[1],
+                           p_test_mm[2] / tvs[2] );
       // On saute les bords car interpolation imparfaite
-      if( 0 <= p_test_vox[0] && p_test_vox[0] < _splineTest.dimX() &&
-          0 <= p_test_vox[1] && p_test_vox[1] < _splineTest.dimY() &&
-          0 <= p_test_vox[2] && p_test_vox[2] < _splineTest.dimZ() )
+      if( 0 <= p_test_vox[0] && p_test_vox[0] < sdim[0] &&
+          0 <= p_test_vox[1] && p_test_vox[1] < sdim[1] &&
+          0 <= p_test_vox[2] && p_test_vox[2] < sdim[2] )
       {
         this->_selection.insert(a, p_test_mm);
       }
@@ -155,15 +163,17 @@ void DataModel::update( vector<float>& p )
 {
   //  if (p.size() != 1000) return;
 
+  vector<int> dims = _deformation.getSize();
+
   // Methode ASSERT a remplacer par une exception
-  ASSERT( p.size() == _dimParam * _deformation.dimX() * _deformation.dimY() * _deformation.dimZ());
+  ASSERT( p.size() == size_t(_dimParam * dims[0] * dims[1] * dims[2]));
   ASSERT( p.size() == _current.size() );
 
   _current = p;
 
-  for( int k = 0, pi = 0; k < _deformation.dimZ(); k++ )
-  for( int j = 0; j < _deformation.dimY(); j++ )
-  for( int i = 0; i < _deformation.dimX(); i++ )
+  for( int k = 0, pi = 0; k < dims[2]; k++ )
+  for( int j = 0; j < dims[1]; j++ )
+  for( int i = 0; i < dims[0]; i++ )
   {
     Point3df cp;
     cp[0] = ( _is2d[0] ? 0. : p[pi++] );
@@ -184,19 +194,22 @@ void DataModel::initBound( const Point3df & maxdef )
 
 void DataModel::updateBound()
 {
-  _lowerBound.resize( _dimParam * _deformation.dimX() * _deformation.dimY() * _deformation.dimZ() );
-  _upperBound.resize( _dimParam * _deformation.dimX() * _deformation.dimY() * _deformation.dimZ() );
+  vector<int> dims = _deformation.getSize();
+
+  _lowerBound.resize( _dimParam * dims[0] * dims[1] * dims[2] );
+  _upperBound.resize( _dimParam * dims[0] * dims[1] * dims[2] );
   Point3df def;
-  def[0] = ( _maxDef[0] >= 0 ? _maxDef[0] : _deformation.sizeX() );
-  def[1] = ( _maxDef[1] >= 0 ? _maxDef[1] : _deformation.sizeY() );
-  def[2] = ( _maxDef[2] >= 0 ? _maxDef[2] : _deformation.sizeZ() );
+  vector<float> vs = _deformation.getVoxelSize();
+  def[0] = ( _maxDef[0] >= 0 ? _maxDef[0] : vs[0] );
+  def[1] = ( _maxDef[1] >= 0 ? _maxDef[1] : vs[1] );
+  def[2] = ( _maxDef[2] >= 0 ? _maxDef[2] : vs[2] );
 
   std::cout << "max def: " << def << std::endl;
 
   int l = 0;
-  for( int k = 0; k < _deformation.dimZ(); ++k )
-  for( int j = 0; j < _deformation.dimY(); ++j )
-  for( int i = 0; i < _deformation.dimX(); ++i )
+  for( int k = 0; k < dims[2]; ++k )
+  for( int j = 0; j < dims[1]; ++j )
+  for( int i = 0; i < dims[0]; ++i )
   for( int c = 0; c < 3; ++c )
   {
     if( !_deformation.isFlat(c) ) {
@@ -246,9 +259,10 @@ bool  DataModel::evalCrit(const Point3df & pTest, const short & value)
 {
   // p = g(p_rig) i.e. transformed point
   Point3df pDef = _deformation.transform( pTest );
-  double pX = pDef[0] / _test.sizeX();
-  double pY = pDef[1] / _test.sizeY();
-  double pZ = pDef[2] / _test.sizeZ();
+  vector<float> vs = _test->getVoxelSize();
+  double pX = pDef[0] / vs[0];
+  double pY = pDef[1] / vs[1];
+  double pZ = pDef[2] / vs[2];
 
   //--------------------------------------//
   // Compute the interpoled Test value    //
@@ -291,22 +305,23 @@ bool  DataModel::evalCrit(const Point3df & pTest, const short & value)
   kSplineTestUpp[2] = ( _is2d[2] ? 0 : kSplineTestUpp[2] );
 
 
+  vector<int> tdim = _splineTest->getSize();
   // resample test image
   for ( int k = kSplineTestLow[2]; k <= kSplineTestUpp[2]; ++k )
   {
     bk3 = ( _is2d[2] ? 1. : spline3(pZ - k) );
     dbk3 = ( _is2d[2] ? 0. : spline3derivative(pZ - k) );
-    ck = getFold( k, _splineTest.dimZ() );
+    ck = getFold( k, tdim[2] );
     for ( int j = kSplineTestLow[1]; j <= kSplineTestUpp[1]; ++j )
     {
       bj3 = ( _is2d[1] ? 1. : spline3(pY - j) );
       dbj3 = ( _is2d[1] ? 0. : spline3derivative(pY - j) );
-      cj = getFold( j, _splineTest.dimY() );
+      cj = getFold( j, tdim[1] );
       for ( int i = kSplineTestLow[0]; i <= kSplineTestUpp[0]; ++i )
       {
         bi3 = ( _is2d[0] ? 1. : spline3(pX - i) );
         dbi3 = ( _is2d[0] ? 0. : spline3derivative(pX - i) );
-        ci = getFold( i, _splineTest.dimX() );
+        ci = getFold( i, tdim[0] );
         st = _splineTest( ci, cj, ck );
 
         t += st * bi3 * bj3 * bk3;
@@ -316,9 +331,10 @@ bool  DataModel::evalCrit(const Point3df & pTest, const short & value)
       }
     }
   }
-  gradTestX /= _splineTest.sizeX();
-  gradTestY /= _splineTest.sizeY();
-  gradTestZ /= _splineTest.sizeZ();
+  vector<float> tvs = _splineTest->getVoxelSize();
+  gradTestX /= tvs[0];
+  gradTestY /= tvs[1];
+  gradTestZ /= tvs[2];
 
   int kappa  = _pdf.getKappa( value );
   int lambda = _pdf.getLambda( t );
@@ -389,35 +405,36 @@ bool  DataModel::evalCrit(const Point3df & pTest, const short & value)
 
 void DataModel::writeDebugTestSpline( const std:: string & filename ) const
 {
-  Writer<AimsData<double> > w( filename );
+  Writer<VolumeRef<double> > w( filename );
   w << _splineTest;
 }
 
 void DataModel::writeDebugContrib( const std::string & filename ) const
 {
-  Writer<AimsData<short> > w( filename );
+  Writer<VolumeRef<short> > w( filename );
   w << _contrib;
 }
 
 void DataModel::writeDebugDerMI( const std::string & filename ) const
 {
-  AimsData<float> der( _deformation.dimX(),
-                       _deformation.dimY(),
-                       _deformation.dimZ(), 3 );
-  der.setSizeXYZT( _deformation.sizeX(), _deformation.sizeY(), _deformation.sizeZ() );
+  vector<int> dims = _deformation.getSize();
+  VolumeRef<float> der( dims[0],
+                        dims[1],
+                        dims[2], 3 );
+  der.header().setProperty( "voxel_size", _deformation.getVoxelSize() );
   der = 0.;
 
   int ind = 0;
-  for( int k = 0; k < _deformation.dimZ(); ++k )
-  for( int j = 0; j < _deformation.dimY(); ++j )
-  for( int i = 0; i < _deformation.dimX(); ++i )
+  for( int k = 0; k < dims[2]; ++k )
+  for( int j = 0; j < dims[1]; ++j )
+  for( int i = 0; i < dims[0]; ++i )
   for( int c = 0; c < 3; ++c )
   {
     if( !_is2d[c] )
       der(i, j, k, c) = _partDer[ind++];
   }
 
-  Writer<AimsData<float> > w( filename );
+  Writer<VolumeRef<float> > w( filename );
   w.write( der );
 
 }

aimsalgo/src/AimsRegisterFFDEstimate/dataModel.h
@@ -39,8 +39,8 @@ class DataModel
     /// \param write_file  Write spline coefficient image as
     ///                    "path/dumpSplineTest.ima"
     DataModel( aims::BucketMap< Void >&            s,
-                AimsData< short >&                 r,
-                AimsData< short >&                 t,
+                carto::VolumeRef< short >          r,
+                carto::VolumeRef< short >          t,
                 Motion&                            m,
                 aims::SplineFfd&                    d,
                 ParzenProbDensFunction&            pdf,
@@ -92,7 +92,7 @@ class DataModel
     double spline3derivative( double x ) const { return _spline.derivative(x); }
   private:
     std::vector<float>         _current;       ///< Param
-    AimsData<double>           _splineTest;    ///< B-Spline coefficients for the test image
+    carto::VolumeRef<double>   _splineTest;    ///< B-Spline coefficients for the test image
     aims::SplineFfd&            _deformation;   ///< FFD transformation
     Motion&                    _rigid;         ///< Pre computed affine motion
     ParzenProbDensFunction&    _pdf;
@@ -106,8 +106,8 @@ class DataModel
     std::vector<float>      _partDer;       ///< dMI/dParam
     std::vector<float>      _upperBound;    ///< parameters bound for optimization
     std::vector<float>      _lowerBound;    ///< parameters bound for optimization
-    AimsData<int16_t> &     _reference;     ///< reference image
-    AimsData<int16_t> &     _test;          ///< test image
+    carto::VolumeRef<int16_t> & _reference;     ///< reference image
+    carto::VolumeRef<int16_t> & _test;          ///< test image
     Point3df                _maxDef;        ///< maximum deformation allowed in mm
 
     // 2d case
@@ -115,7 +115,7 @@ class DataModel
     long                    _dimParam;      ///< FFD dimension (1d/2d/3d)
 
     // debug
-    AimsData<short>         _contrib;
+    carto::VolumeRef<short> _contrib;
 };
 
 #endif

commit 6add5b72d173e2f8ef0c2667288966a9d7b8905a

Fix resampling of 4D volumes
Depending on the voxel size along the fourth dimension, the last channels of a
4D volume could be skipped by the resamplers. This bug was introduced in
4b34de2139f86354c2cd6db4348a06c97e108156 (SVN r82911).

aimsalgo/src/aimsalgo/resampling/resampler_d.h
@@ -84,8 +84,8 @@ resample_inv_to_vox( const carto::Volume< T >& inVolume,
   int outSizeY = sz[1];
   int outSizeZ = sz[2];
   int outSizeT = sz[3];
-  if( outSizeT > inVolume.getVoxelSize()[3] )
-    outSizeT = inVolume.getVoxelSize()[3];
+  if( outSizeT > inVolume.getSizeT() )
+    outSizeT = inVolume.getSizeT();
 
   T* o;
 

pyaimsalgo/python/soma/aimsalgo/tests/test_resampling.py
@@ -121,6 +121,14 @@ class ResamplingTestCase(unittest.TestCase):
             np.int16
         )
 
+    def test_4d_resampling(self):
+        resampler = aimsalgo.NearestNeighborResampler_S16()
+        ref = create_reference_volume(np.int16, shape=(1, 1, 1, 2))
+        out = empty_volume_like(ref)
+        resampler.setRef(ref)
+        resampler.doit(identity_transform, out)
+        self.assertTrue(np.array_equal(out, ref))
+
     # known issue: the last element along each axis is -32768 (the mask value)
     def test_identity_masklin_resampling_int16_except_last_elem(self):
         self.do_identity_resampling_test(