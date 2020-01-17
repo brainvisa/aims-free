b99bd21f8e3ce1693d3d09af4d4cb616ac3e7cc5
Denis Rivière <denis.riviere.gm@gmail.com>
2020-01-17 17:05:13 +0100

    pass an apparently needed additional parameter to boost sparse_matrix::resize()

--- aimsalgo/src/aimsalgo/mesh/curv.cc
+++ aimsalgo/src/aimsalgo/mesh/curv.cc
@@ -1064,7 +1064,7 @@ namespace aims
     cout << "makeLaplacianMatrix... " << weights.size() << endl;
     float w = 0;
     unsigned i, j;
-    lmat.resize( weights.size(), weights.size() );
+    lmat.resize( weights.size(), weights.size(), false );
     LaplacianWeights::const_iterator iw, ew = weights.end();
 
     for( iw=weights.begin(); iw!=ew; ++iw )
