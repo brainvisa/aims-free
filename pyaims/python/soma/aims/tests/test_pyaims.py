d67efb6f7a78c0812402c2214420a60c7e08b1c6
Denis Rivière <denis.riviere.gm@gmail.com>
2022-06-17 12:21:54 +0200

    use a lighter test file, fixed test suite (notebook tests were disabled)

--- pyaims/python/soma/aims/tests/test_pyaims.py
+++ pyaims/python/soma/aims/tests/test_pyaims.py
@@ -1,6 +1,5 @@
 from __future__ import print_function
 
-from __future__ import absolute_import
 import unittest
 import doctest
 import zipfile
@@ -8,8 +7,9 @@ import os
 from shutil import rmtree
 import tempfile
 import sys
-
 import six.moves.urllib.request
+
+from soma.test_utils import test_notebook as tnb
 
 
 def setup_doctest(test):
@@ -18,24 +18,35 @@ def setup_doctest(test):
         tests_dir = tempfile.gettempdir()
     tests_dir = os.path.join(tests_dir, "tmp_tests_brainvisa")
     if not os.path.exists(tests_dir):
-        os.mkdir(tests_dir)
+        os.makedirs(tests_dir)
     os.chdir(tests_dir)
-    if not os.path.exists("demo_data.zip"):
-        print("Download ftp://ftp.cea.fr/pub/dsv/anatomist/data/demo_data.zip "
+    if not os.path.exists("test_data.zip"):
+        print("Download ftp://ftp.cea.fr/pub/dsv/anatomist/data/test_data.zip "
               "to ", tests_dir)
         six.moves.urllib.request.urlretrieve(
-            "ftp://ftp.cea.fr/pub/dsv/anatomist/data/demo_data.zip",
-            "demo_data.zip")
+            "ftp://ftp.cea.fr/pub/dsv/anatomist/data/test_data.zip",
+            "test_data.zip")
     if os.path.exists("data_for_anatomist"):
         rmtree("data_for_anatomist")
     if os.path.exists("data_unprocessed"):
         rmtree("data_unprocessed")
-    zf = zipfile.ZipFile("demo_data.zip")
+    zf = zipfile.ZipFile("test_data.zip")
     zf.extractall()
 
 
 def teardown_doctest(test):
     os.chdir(test.globs['curdir'])
+
+
+class PyaimsNBTest(unittest.TestCase):
+    @unittest.skipIf(tnb.main_jupyter is None, 'Notebook tests not supported')
+    def test_pyaims_notebook(self):
+        # try the notebook version if it can be processed on this system
+        print('testing notebook')
+        tnb.test_notebook(
+            os.path.join(os.path.dirname(sys.argv[0]),
+                          'pyaims_tutorial_nb.ipynb'),
+            timeout=300)
 
 
 def test_suite():
@@ -49,29 +60,11 @@ def test_suite():
                              tearDown=teardown_doctest,
                              optionflags=doctest.ELLIPSIS))
     suite.addTest(doctest_suite)
+    loader = unittest.defaultTestLoader
+    tests = loader.loadTestsFromTestCase(PyaimsNBTest)
+    suite.addTests(tests)
     return suite
 
-if __name__ == '__main__':
-    # try the notebook version if it can be processed on this system
-    from soma.test_utils import test_notebook as tnb
-    try:
-        raise Warning('no notebook')
-        if tnb.test_notebook(
-            os.path.join(os.path.dirname(sys.argv[0]),
-                         'pyaims_tutorial_nb.ipynb'),
-            timeout=300):
-            sys.exit(0)
-        else:
-            sys.exit(1)
-    except Warning:
-        #verbosity=0
-        #if '-v' in sys.argv:
-            #i = sys.argv.index('-v')
-            #verbosity = int(sys.argv[i + 1])
-            #sys.argv = sys.argv[:i] + sys.argv[i+2:]
-        #elif '--verbose' in sys.argv:
-            #i = sys.argv.index('--verbose')
-            #verbosity = int(sys.argv[i + 1])
-            #sys.argv = sys.argv[:i] + sys.argv[i+2:]
 
-        unittest.main(defaultTest='test_suite')
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
