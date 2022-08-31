1bcde6132ffda05fee56f9a6693a6ec4d9908e2f
Denis Rivière <denis.riviere.gm@gmail.com>
2022-08-31 18:08:46 +0200

    fill the FFD header when loading it

--- aimsalgo/src/aimsalgo/registration/ffd.cc
+++ aimsalgo/src/aimsalgo/registration/ffd.cc
@@ -866,6 +866,10 @@ Point3dd TrilinearFfd::transformDouble( double x, double y, double z ) const
     if( res )
     {
       obj.updateDimensions();
+      cout << "set FFD header\n";
+      VolumeRef<Point3df> rvol( (rc_ptr<Volume<Point3df> > ) obj );
+      cout << "hdr size: " << rvol->header().size() << endl;
+      obj.setHeader( Object::reference( rvol->header() ) );
     }
     return res;
   }
