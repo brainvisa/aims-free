c38f2e5a4f0f4f89648c2025c582467231a87531
Denis Rivière <denis.riviere.gm@gmail.com>
2023-02-03 16:17:34 +0100

    complement doc

--- aimsdata/src/aimsdata/data/headerutil.h
+++ aimsdata/src/aimsdata/data/headerutil.h
@@ -42,13 +42,22 @@ namespace carto
   /** force or alter a specific voxels orientation on disk (if the output
   format supports it), by changing the internal "storage_to_memory" matrix,
   in a more or less convenient way, with different syntaxes:
+
   - "flipx yflip" : flips orientations of x and y axes.
-    "neuro", "neurological" or "left-to-right": force neuro orientation.
+  -"neuro", "neurological" or "left-to-right": force neuro orientation.
   - "radio", "radiological" or "right-to-left": force radio orientation.
   - "-1 1 -1": set a flip matrix flipping x and z axes (set the diagonal).
   - "-1 0 0  0 1 0  0 0 -1": same by specifying the full rotation matrix.
   - "-1 0 0 255  0 1 0 0  0 0 -1 123  0 0 0 1": same by specifying
     the full 4x4 matrix.
+
+  By default, all orientation matrices are applied relatively to the current
+  voxels orientation. To set absolute matrices (from the AIMS conventional
+  orientation), specify "abs:" before the matrix. Ex:
+
+  - "abs: -1 1 -1"
+  - "abs: -1 0 0 255  0 1 0 0  0 0 -1 123  0 0 0 1"
+
   */
   void setOrientationInformation( carto::Object hdr,
                                   const std::string & orient );
