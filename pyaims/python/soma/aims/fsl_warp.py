7994ca332815c1ea04ab4539a00fbf0529be27e4
Denis Rivière <denis.riviere.gm@gmail.com>
2023-05-30 10:42:21 +0200

    allow to write estimated affine transformation from a FSL warping field

--- pyaims/python/soma/aims/fsl_warp.py
+++ pyaims/python/soma/aims/fsl_warp.py
@@ -88,6 +88,7 @@ def fsl_to_aims_warp_field(W, input_hdr, output_hdr):
                      np.abs(S2Mo.inverse().transformVector(
                         aims.Point3df(output_hdr['voxel_size'][:3]))))
     S2Mo_inv = S2Mo.inverse()
+
     # We cannot write "W + VSoLAS" etc. W in the math is a transform function
     # so we write WLPI as a function first
     #def _WLPI(left, VSoLAS, VSo, S2Mo_inv, W, p):
@@ -97,7 +98,7 @@ def fsl_to_aims_warp_field(W, input_hdr, output_hdr):
             #- VSo.transform(p.np.astype(float))
         #return res
 
-    # re-write WLPI using np matrix operations to avoid slow loops
+    # re-write _WLPI using np matrix operations to avoid slow loops
     def _WLPI2(left, VSoLAS, VSo, S2Mo_inv, W):
         # print('WLPI2\ngenerate indices...')
         print('0%', end='')
@@ -209,12 +210,27 @@ in AIMS programs as a FfdTransformation.
     parser.add_argument(
         '-i', '--input',
         help='input FSL warp field (must not be a coefficients file)')
-    parser.add_argument('-o', '--output', help='output AIMS FFD field')
+    parser.add_argument(
+        '-o', '--output', help='output AIMS FFD field. If omitted, the output '
+        'field will not be written, which can make sense if -a is specified.')
     parser.add_argument('-s', '--source', help='source input volume space')
     parser.add_argument(
         '-r', '--reference',
         help='output reference volume (may be the field -i itself, '
         'the default)')
+    parser.add_argument(
+        '-a', '--affine',
+        help='output estimated affine transformation (.trm). Remember that '
+        'the FSL field is the inverse of what is "said": from where a point '
+        'is pulled to get to destination space, so the affine is also the '
+        'inverse (destination to source space).')
+    parser.add_argument(
+        '--without_affine',
+        help='optional input FSL warp field without the affine part (use '
+        'fnirtfileutils without -a option). Only used to estimate the affine '
+        'transformation (with -a option here). If provided, the difference '
+        'between warp fields with and without the affine part will be used to '
+        'estimate the affine field, which is more accurate and more robust.')
     options = parser.parse_args()
 
     input_name = options.input
@@ -223,6 +239,16 @@ in AIMS programs as a FfdTransformation.
     source_name = options.source
     if ref_name is None:
         ref_name = input_name
+    affine_name = options.affine
+    input_wo_affine_name = None
+    input_wo_affine_name = options.without_affine
+    if not output_name and not affine_name:
+        print('Warning: no output specified - nothing to do.')
+        return
+    if input_wo_affine_name and not affine_name:
+        print('Warning: without_affine is specified, but affine output is not '
+              'given, without_affine will not be used.')
+
     W = aims.read(input_name)
     finder = aims.Finder()
     finder.check(ref_name)
@@ -231,7 +257,24 @@ in AIMS programs as a FfdTransformation.
     input_hdr = finder.header()
 
     WLPI = fsl_to_aims_warp_field(W, input_hdr, output_hdr)
-    aims.write(WLPI, output_name)
+    if output_name:
+        aims.write(WLPI, output_name)
+
+    if affine_name:
+        if input_wo_affine_name:
+            print('reading and transforming field without affine...')
+            WNA = aims.read(input_wo_affine_name)
+            WNALPI = fsl_to_aims_warp_field(WNA, input_hdr, output_hdr)
+            WA = WLPI - WNALPI
+        else:
+            WA = WLPI
+        affine, aff_std = affine_estimate_from_aims_field(WA)
+        print('affine std:')
+        print(aff_std)
+        std_max = np.max(aff_std)
+        if  std_max> 0.1:
+            print('Warning: the affine estimate is bad.')
+        aims.write(affine, affine_name)
 
 
 if __name__ == '__main__':
