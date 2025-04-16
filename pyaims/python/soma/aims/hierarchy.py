6a7d7485986b0beb97ecabd9eb56ea0496e6a549
Denis Rivière <denis.riviere.gm@gmail.com>
2025-04-16 22:11:48 +0200

    fixed a possible reference to deleted objects problem:
    prevent tree deletion when children are still referenced in python

--- pyaims/python/soma/aims/hierarchy.py
+++ pyaims/python/soma/aims/hierarchy.py
@@ -29,9 +29,11 @@
 #
 # The fact that you are presently reading this means that you have had
 # knowledge of the CeCILL-B license and that you accept its terms.
-from __future__ import absolute_import
+
 import numpy
 import sys
+
+
 aims = sys.modules['soma.aims']
 
 __all__ = []
