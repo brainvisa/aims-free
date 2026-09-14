cdf87f26301da259278c1d7076b9fcc1ebdb3b06
sapetnioc <sapetnioc@users.noreply.github.com>
2026-09-14 10:25:21 +0200

    WIP fixing compilation errors

--- pyaims/src/libpyaims/data/volumeoperators.h
+++ pyaims/src/libpyaims/data/volumeoperators.h
@@ -6,288 +6,165 @@
 namespace
 {
 
-  // Helper template to deduce result type and apply operation
-  template <typename Op, typename L, typename R>
-  auto apply_operation(L& l, R& r, Op op)
-    -> decltype(op(l, r))
-  {
-    return op(l, r);
-  }
+  // // Helper template to deduce result type and apply operation
+  // template <typename Op, typename L, typename R>
+  // auto apply_operation(L& l, R& r, Op op)
+  //   -> decltype(op(l, r))
+  // {
+  //   return op(l, r);
+  // }
 
   // Specialization for Volume types to handle VolumeRef
   template <typename Op, typename L, typename R>
-  auto apply_volume_operation(L& l, R& r, Op op)
+  auto inline apply_volume_operation(L& l, R& r, Op op)
     -> decltype(op(l, r))
   {
     carto::VolumeRef<typename std::decay<decltype(l)>::type::datatype> rl(&l);
     carto::VolumeRef<typename std::decay<decltype(r)>::type::datatype> rr(&r);
-    auto rres = op(rl, rr);
+    auto rres = apply(rl, rr);
     auto res = rres.get();
     rres.release();
     return res;
   }
 
-  // __add__ implementation
-  template <typename L, typename R>
+
+  template <typename T, typename U>
   inline
-  auto __add__( L & l, R & r )
+  auto __add__( 
+    carto::Volume<T> & l, const U & r )
   {
-    return apply_operation(l, r, [](auto& left, auto& right) {
-      return left + right;
-    });
+    return apply_volume_operation(l, r, [](auto &x, auto &y) {return x + y;});
   }
 
-  template <>
+  template <typename T, typename U>
   inline
-  carto::Volume<float> * __add__<carto::Volume<float>, carto::Volume<float>>( 
-    carto::Volume<float> & l, carto::Volume<float> & r )
-  {
-    auto op = [](auto& left, auto& right) { return left + right; };
-    carto::VolumeRef<float> rl(&l);
-    carto::VolumeRef<float> rr(&r);
-    auto rres = op(rl, rr);
-    auto res = rres.get();
-    rres.release();
-    return res;
-  }
-
-  // __iadd__ implementation
-  template <typename L, typename R>
-  inline
-  L & __iadd__( L & l, R & r )
+  carto::Volume<T> &__iadd__( 
+    carto::Volume<T> & l, const U & r )
   {
     l += r;
     return l;
   }
 
-  // __sub__ implementation
-  template <typename L, typename R>
+
+  template <typename T, typename U>
   inline
-  auto __sub__( L & l, R & r )
+  auto __sub__( 
+    carto::Volume<T> & l, const U & r )
   {
-    return apply_operation(l, r, [](auto& left, auto& right) {
-      return left - right;
-    });
+    return apply_volume_operation(l, r, [](auto &x, auto &y) {return x - y;});
   }
 
-  template <>
+  template <typename T, typename U>
   inline
-  carto::Volume<float> * __sub__<carto::Volume<float>, carto::Volume<float>>( 
-    carto::Volume<float> & l, carto::Volume<float> & r )
-  {
-    auto op = [](auto& left, auto& right) { return left - right; };
-    carto::VolumeRef<float> rl(&l);
-    carto::VolumeRef<float> rr(&r);
-    auto rres = op(rl, rr);
-    auto res = rres.get();
-    rres.release();
-    return res;
-  }
-
-  // __isub__ implementation
-  template <typename L, typename R>
-  inline
-  L & __isub__( L & l, R & r )
+  carto::Volume<T> &__isub__( 
+    carto::Volume<T> & l, const U & r )
   {
     l -= r;
     return l;
   }
 
-  // __mul__ implementation
-  template <typename L, typename R>
+
+  template <typename T, typename U>
   inline
-  auto __mul__( L & l, R & r )
+  auto __mul__( 
+    carto::Volume<T> & l, const U & r )
   {
-    return apply_operation(l, r, [](auto& left, auto& right) {
-      return left * right;
-    });
+    return apply_volume_operation(l, r, [](auto &x, auto &y) {return x * y;});
   }
 
-  template <>
+  template <typename T, typename U>
   inline
-  carto::Volume<float> * __mul__<carto::Volume<float>, carto::Volume<float>>( 
-    carto::Volume<float> & l, carto::Volume<float> & r )
-  {
-    auto op = [](auto& left, auto& right) { return left * right; };
-    carto::VolumeRef<float> rl(&l);
-    carto::VolumeRef<float> rr(&r);
-    auto rres = op(rl, rr);
-    auto res = rres.get();
-    rres.release();
-    return res;
-  }
-
-  // __imul__ implementation
-  template <typename L, typename R>
-  inline
-  L & __imul__( L & l, R & r )
+  carto::Volume<T> &__imul__( 
+    carto::Volume<T> & l, const U & r )
   {
     l *= r;
     return l;
   }
 
-  // __div__ implementation
-  template <typename L, typename R>
+  template <typename T, typename U>
   inline
-  auto __div__( L & l, R & r )
+  auto __div__( 
+    carto::Volume<T> & l, const U & r )
   {
-    return apply_operation(l, r, [](auto& left, auto& right) {
-      return left / right;
-    });
+    return apply_volume_operation(l, r, [](auto &x, auto &y) {return x / y;});
   }
 
-  template <>
+  template <typename T, typename U>
   inline
-  carto::Volume<float> * __div__<carto::Volume<float>, carto::Volume<float>>( 
-    carto::Volume<float> & l, carto::Volume<float> & r )
-  {
-    auto op = [](auto& left, auto& right) { return left / right; };
-    carto::VolumeRef<float> rl(&l);
-    carto::VolumeRef<float> rr(&r);
-    auto rres = op(rl, rr);
-    auto res = rres.get();
-    rres.release();
-    return res;
-  }
-
-  // __idiv__ implementation
-  template <typename L, typename R>
-  inline
-  L & __idiv__( L & l, R & r )
-  {
-    l /= r;
-    return l;
-  }
-
-  // __mod__ implementation
-  template <typename L, typename R>
-  inline
-  auto __mod__( L & l, R & r )
-  {
-    return apply_operation(l, r, [](auto& left, auto& right) {
-      return left % right;
-    });
-  }
-
-  template <>
-  inline
-  carto::Volume<float> * __mod__<carto::Volume<float>, carto::Volume<float>>( 
-    carto::Volume<float> & l, carto::Volume<float> & r )
-  {
-    auto op = [](auto& left, auto& right) { return left % right; };
-    carto::VolumeRef<float> rl(&l);
-    carto::VolumeRef<float> rr(&r);
-    auto rres = op(rl, rr);
-    auto res = rres.get();
-    rres.release();
-    return res;
-  }
-
-  // __imod__ implementation
-  template <typename L, typename R>
-  inline
-  L & __imod__( L & l, R & r )
-  {
-    l %= r;
-    return l;
-  }
-
-  // __and__ implementation
-  template <typename L, typename R>
-  inline
-  auto __and__( L & l, R & r )
-  {
-    return apply_operation(l, r, [](auto& left, auto& right) {
-      return left & right;
-    });
-  }
-
-  template <>
-  inline
-  carto::Volume<int> * __and__<carto::Volume<int>, carto::Volume<int>>( 
-    carto::Volume<int> & l, carto::Volume<int> & r )
-  {
-    auto op = [](auto& left, auto& right) { return left & right; };
-    carto::VolumeRef<int> rl(&l);
-    carto::VolumeRef<int> rr(&r);
-    auto rres = op(rl, rr);
-    auto res = rres.get();
-    rres.release();
-    return res;
-  }
-
-  // __iand__ implementation
-  template <typename L, typename R>
-  inline
-  L & __iand__( L & l, R & r )
+  carto::Volume<T> &__idiv__( 
+    carto::Volume<T> & l, const U & r )
   {
     l &= r;
     return l;
   }
 
-  // __or__ implementation
-  template <typename L, typename R>
+  template <typename T, typename U>
   inline
-  auto __or__( L & l, R & r )
+  auto __mod__( 
+    carto::Volume<T> & l, const U & r )
   {
-    return apply_operation(l, r, [](auto& left, auto& right) {
-      return left | right;
-    });
+    return apply_volume_operation(l, r, [](auto &x, auto &y) {return x % y;});
   }
 
-  template <>
+  template <typename T, typename U>
   inline
-  carto::Volume<int> * __or__<carto::Volume<int>, carto::Volume<int>>( 
-    carto::Volume<int> & l, carto::Volume<int> & r )
+  carto::Volume<T> &__imod__( 
+    carto::Volume<T> & l, const U & r )
   {
-    auto op = [](auto& left, auto& right) { return left | right; };
-    carto::VolumeRef<int> rl(&l);
-    carto::VolumeRef<int> rr(&r);
-    auto rres = op(rl, rr);
-    auto res = rres.get();
-    rres.release();
-    return res;
+    l %= r;
+    return l;
   }
 
-  // __ior__ implementation
-  template <typename L, typename R>
+  template <typename T, typename U>
   inline
-  L & __ior__( L & l, R & r )
+  auto __and__( 
+    carto::Volume<T> & l, const U & r )
+  {
+    return apply_volume_operation(l, r, [](auto &x, auto &y) {return x & y;});
+  }
+
+  template <typename T, typename U>
+  inline
+  carto::Volume<T> &__iand__( 
+    carto::Volume<T> & l, const U & r )
+  {
+    l &= r;
+    return l;
+  }
+
+
+  template <typename T, typename U>
+  inline
+  auto __or__( 
+    carto::Volume<T> & l, const U & r )
+  {
+    return apply_volume_operation(l, r, [](auto &x, auto &y) {return x | y;});
+  }
+
+  template <typename T, typename U>
+  inline
+  carto::Volume<T> &__ior__( 
+    carto::Volume<T> & l, const U & r )
   {
     l |= r;
     return l;
   }
 
-  // __xor__ implementation
-  template <typename L, typename R>
+
+  template <typename T, typename U>
   inline
-  auto __xor__( L & l, R & r )
+  auto __xor__( 
+    carto::Volume<T> & l, const U & r )
   {
-    return apply_operation(l, r, [](auto& left, auto& right) {
-      return left ^ right;
-    });
+    return apply_volume_operation(l, r, [](auto &x, auto &y) {return x ^ y;});
   }
 
-  template <>
+  template <typename T, typename U>
   inline
-  carto::Volume<int> * __xor__<carto::Volume<int>, carto::Volume<int>>( 
-    carto::Volume<int> & l, carto::Volume<int> & r )
+  carto::Volume<T> &__ixor__( 
+    carto::Volume<T> & l, const U & r )
   {
-    auto op = [](auto& left, auto& right) { return left ^ right; };
-    carto::VolumeRef<int> rl(&l);
-    carto::VolumeRef<int> rr(&r);
-    auto rres = op(rl, rr);
-    auto res = rres.get();
-    rres.release();
-    return res;
-  }
-
-  // __ixor__ implementation
-  template <typename L, typename R>
-  inline
-  L & __ixor__( L & l, R & r )
-  {
-    l ^= r;
+    l  ^= r;
     return l;
   }
 
